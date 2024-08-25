43e517254fee729b9e1c53bcf74a9058c2027dd6
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-25 21:23:41 +0600

    feat: Add ASidebar component for admin page sidebar navigation

---
+++ src/components/admin/layout/ASidebar.tsx
@@ -0,0 +1,66 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { adminSideBar } from "@/constants";
+import Link from "next/link";
+
+const ASidebar = () => {
+  return (
+    <div className="bg-slate-800 h-[calc(100svh-60px)] py-4 px-3 text-slate-50">
+      <Accordion type="single" collapsible>
+        {adminSideBar.map((item) => {
+          if (item.hasSubMenu) {
+            return (
+              <AccordionItem
+                key={item.name}
+                value={item.name}
+                className="border-none"
+              >
+                <AccordionTrigger className="p-3 font-semibold no-focus flex justify-between items-center rounded-lg hover:bg-primary-hover hover:no-underline data-[state=open]:bg-slate-50 data-[state=open]:text-primary-hover">
+                  <Link href={item.link} className="no-focus flex items-center">
+                    <div className="mr-3">
+                      <item.icon className="size-5" />
+                    </div>
+                    {item.name}
+                  </Link>
+                </AccordionTrigger>
+
+                <AccordionContent className="ml-5 border-l border-slate-500 text-base">
+                  {item.subMenus?.map((subItem) => (
+                    <div key={subItem.name}>
+                      <Link
+                        href={subItem.link}
+                        className="px-3 py-2 pl-5 inline-block no-focus w-full border-l-2 border-transparent hover:border-primary hover:text-primary transition-all"
+                      >
+                        {subItem.name}
+                      </Link>
+                    </div>
+                  ))}
+                </AccordionContent>
+              </AccordionItem>
+            );
+          } else {
+            return (
+              <div key={item.name}>
+                <Link
+                  href={item.link}
+                  className="no-focus flex items-center font-semibold rounded-lg hover:bg-primary-hover"
+                >
+                  <div className="p-3">
+                    <item.icon className="size-5" />
+                  </div>
+                  {item.name}
+                </Link>
+              </div>
+            );
+          }
+        })}
+      </Accordion>
+    </div>
+  );
+};
+
+export default ASidebar;
