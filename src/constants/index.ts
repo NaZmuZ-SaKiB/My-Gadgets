21c6189a3c1d418d85b2f94677fe80b133339a4c
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-25 20:11:59 +0600

    feat: Add backend URL and user role constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,9 @@
 export * from "./assets";
+
+export const backendUrl = process.env.BACKEND_URL;
+export const authKey = "mg-jwt";
+export const USER_ROLE = {
+  SUPER_ADMIN: "super_admin",
+  ADMIN: "admin",
+  USER: "user",
+} as const;
