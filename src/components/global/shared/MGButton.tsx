26569bc000ccefdee6eec4a32adaa248a11e4ef8
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-25 19:21:12 +0600

    feat: Add MGButton component for buttons

---
+++ src/components/global/shared/MGButton.tsx
@@ -0,0 +1,35 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { VariantProps } from "class-variance-authority";
+import React from "react";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const MGButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, ...props }: ButtonProps, ref) => {
+    return (
+      <Button
+        className={cn(
+          "rounded-xl bg-primary focus-visible:bg-primary-hover focus-visible:text-white hover:bg-primary-hover no-focus",
+          {
+            "bg-transparent border-primbg-primary text-primbg-primary hover:bg-primary hover:text-white":
+              props.variant === "outline",
+          },
+          className
+        )}
+        {...props}
+        ref={ref}
+      >
+        {children}
+      </Button>
+    );
+  }
+);
+
+MGButton.displayName = "MGButton";
+
+export default MGButton;
