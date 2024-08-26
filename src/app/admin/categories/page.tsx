0138245a8a87f5e80ac6bd33f4e0e4df678ec8b6
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-26 12:46:01 +0600

    feat: Add CategoriesData component for displaying category data

--- src/app/admin/categories/page.tsx
+++ src/app/admin/categories/page.tsx
@@ -2,6 +2,7 @@ import AGrid from "@/components/admin/admin-ui/AGrid";
 import APageContainer from "@/components/admin/admin-ui/APageContainer";
 import APageHeading from "@/components/admin/admin-ui/APageHeading";
 import CategoryCreateForm from "./_components/CategoryCreateForm";
+import CategoriesData from "./_components/CategoriesData";
 
 const CategoriesPage = () => {
   return (
@@ -11,7 +12,9 @@ const CategoriesPage = () => {
         <div>
           <CategoryCreateForm />
         </div>
-        <div></div>
+        <div>
+          <CategoriesData />
+        </div>
       </AGrid>
     </APageContainer>
   );
