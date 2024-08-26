6bfb56d645a5951911f8e9f31be67e031cc98550
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-26 14:54:40 +0600

    refactor: Update category actions to include update, toggle featured, toggle show on top menu, and remove actions

--- src/lib/actions/category.action.ts
+++ src/lib/actions/category.action.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { authKey, backendUrl } from "@/constants";
+import { TCategory } from "@/types/category.type";
 import { cookies } from "next/headers";
 
 export const categoryCreateAction = async (payload: {
@@ -9,6 +10,25 @@ export const categoryCreateAction = async (payload: {
 }) => {
   const response = await fetch(`${backendUrl}/api/category`, {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: cookies().get(authKey)?.value || "",
+    },
+    body: JSON.stringify(payload),
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  return result;
+};
+
+export const categoryUpdateAction = async (
+  id: string,
+  payload: Partial<TCategory>
+) => {
+  const response = await fetch(`${backendUrl}/api/category/${id}`, {
+    method: "PATCH",
     headers: {
       "Content-Type": "application/json",
       Authorization: cookies().get(authKey)?.value || "",
@@ -48,6 +68,54 @@ export const categoryGetByIdAction = async (id: string) => {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
+    },
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  return result;
+};
+
+export const categoryToggleFeaturedAction = async (id: string) => {
+  const response = await fetch(`${backendUrl}/api/category/${id}/featured`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: cookies().get(authKey)?.value || "",
+    },
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  return result;
+};
+
+export const categoryToggleShowOnTopMenuAction = async (id: string) => {
+  const response = await fetch(
+    `${backendUrl}/api/category/${id}/show-on-top-menu`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: cookies().get(authKey)?.value || "",
+      },
+      cache: "no-store",
+    }
+  );
+
+  const result = await response.json();
+
+  return result;
+};
+
+export const categoryRemoveAction = async (id: string) => {
+  const response = await fetch(`${backendUrl}/api/category/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: cookies().get(authKey)?.value || "",
     },
     cache: "no-store",
   });
