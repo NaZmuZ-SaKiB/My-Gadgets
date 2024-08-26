9e0a48c4d5abac401f1b94753dda652c8abc5dc9
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-26 21:12:23 +0600

    feat: Add DataSearchBox component for searching data

--- src/app/admin/categories/_components/CategoriesData.tsx
+++ src/app/admin/categories/_components/CategoriesData.tsx
@@ -2,12 +2,18 @@
 
 import { useState } from "react";
 import CategoriesTable from "./CategoriesTable";
+import AFloatingBox from "@/components/admin/admin-ui/AFloatingBox";
+import DataSearchBox from "@/components/admin/shared/filters/DataSearchBox";
 
 const CategoriesData = () => {
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
 
   return (
     <div className="flex flex-col gap-3 h-full">
+      <AFloatingBox>
+        <DataSearchBox />
+      </AFloatingBox>
+
       <CategoriesTable
         selectedCategories={selectedCategories}
         setSelectedCategories={setSelectedCategories}
