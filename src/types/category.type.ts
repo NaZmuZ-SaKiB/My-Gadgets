8cbb0b5715bccd1e0f54e8c4ca6cea975a5e7aa4
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-26 14:54:31 +0600

    feat: Add featured, showOnTopMenu, and image properties to TCategory type

--- src/types/category.type.ts
+++ src/types/category.type.ts
@@ -4,6 +4,9 @@ export type TCategory = {
   _id: string;
   name: string;
   label: string;
+  featured: boolean;
+  showOnTopMenu: boolean;
+  image: string;
 
   createdAt: Date;
   updatedAt: Date;
