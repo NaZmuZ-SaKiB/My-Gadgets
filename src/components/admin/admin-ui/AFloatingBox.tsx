93f3f5ce7784c453007fd31171c167a47df89ffb
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-26 11:00:20 +0600

    feat: Add AFloatingBox component for admin page UI

---
+++ src/components/admin/admin-ui/AFloatingBox.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils";
+import { ClassValue } from "clsx";
+import { ReactNode } from "react";
+
+type TProps = {
+  children?: ReactNode;
+  className?: ClassValue;
+};
+
+const AFloatingBox = ({ children, className }: TProps) => {
+  return (
+    <div
+      className={cn(
+        "p-4 rounded-xl bg-white hover:shadow-xl hover:shadow-gray-200 transition-shadow duration-300",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default AFloatingBox;
