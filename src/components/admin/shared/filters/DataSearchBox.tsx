438a3086da33d1a454e06673b20681253b5b43a9
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-26 22:55:21 +0600

    feat: Update DataSearchBox component to handle search term from URL params

--- src/components/admin/shared/filters/DataSearchBox.tsx
+++ src/components/admin/shared/filters/DataSearchBox.tsx
@@ -7,12 +7,13 @@ import { X } from "lucide-react";
 import MGButton from "@/components/global/shared/MGButton";
 
 const DataSearchBox = () => {
-  const [search, setSearch] = useState<string>("");
   const router = useRouter();
   const pathName = usePathname();
   const searchParams = useSearchParams();
 
   const params = new URLSearchParams(searchParams);
+
+  const [search, setSearch] = useState<string>(params.get("searchTerm") || "");
 
   const handleSearch = () => {
     if (!search || search === params.get("searchTerm")) return;
@@ -41,10 +42,9 @@ const DataSearchBox = () => {
   }, []);
 
   return (
-    <div className="w-full flex gap-2">
-      <div className="shrink-0 flex-1 relative">
+    <div className="w-full flex gap-2 flex-wrap">
+      <div className="shrink-0 flex-1 basis-60 relative">
         <Input
-          defaultValue={params.get("searchTerm") || ""}
           value={search}
           placeholder="Search..."
           className=" w-full pr-8 focus-visible:border-primary no-focus"
