e079e1884701350f5780be41a3eb6a12690b3351
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-26 11:00:52 +0600

    feat: Add category validation for creating a category

---
+++ src/lib/validations/category.validation.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const create = z.object({
+  name: z
+    .string({
+      required_error: "Name is required",
+      invalid_type_error: "Name must be a string",
+    })
+    .min(3, "Name must be at least 3 characters long")
+    .max(20, "Name must be at most 20 characters long"),
+  label: z
+    .string({
+      required_error: "Label is required",
+      invalid_type_error: "Label must be a string",
+    })
+    .min(3, "Label must be at least 3 characters long")
+    .max(25, "Label must be at most 25 characters long"),
+});
+
+export const CategoryValidation = {
+  create,
+};
