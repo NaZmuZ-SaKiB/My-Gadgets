7643643a8a9b2d74226bef05a54f73eae3d8ba87
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-26 12:20:14 +0600

    feat: Add useCategoryGetAllQuery and useCategoryGetByIdQuery hooks for fetching categories

--- src/lib/queries/category.query.ts
+++ src/lib/queries/category.query.ts
@@ -1,7 +1,24 @@
-import { useMutation } from "@tanstack/react-query";
-import { categoryCreateAction } from "../actions/category.action";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+  categoryCreateAction,
+  categoryGetAllAction,
+  categoryGetByIdAction,
+} from "../actions/category.action";
+import { AQTags } from "@/constants/tags";
 
 export const useCategoryCreateMutation = () =>
   useMutation({
     mutationFn: categoryCreateAction,
+  });
+
+export const useCategoryGetAllQuery = (params: string) =>
+  useQuery({
+    queryKey: [AQTags.CATEGORY, AQTags.ALL, params],
+    queryFn: () => categoryGetAllAction(params),
+  });
+
+export const useCategoryGetByIdQuery = (id: string) =>
+  useQuery({
+    queryKey: [AQTags.CATEGORY, id],
+    queryFn: () => categoryGetByIdAction(id),
   });
