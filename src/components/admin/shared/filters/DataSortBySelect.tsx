2b45eb4c6f0dce77578210d28d1b173a72d987be
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-26 21:35:41 +0600

    feat: Add DataSortBySelect component for selecting data sort by option

---
+++ src/components/admin/shared/filters/DataSortBySelect.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+const DataSortBySelect = ({ options }: { options: string[] }) => {
+  const router = useRouter();
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+
+  const params = new URLSearchParams(searchParams);
+
+  const handleChange = (value: string) => {
+    params.delete("sortBy");
+    params.delete("page");
+    params.append("page", "1");
+    params.append("sortBy", value);
+
+    router.replace(`${pathName}?${params}`);
+  };
+
+  return (
+    <Select
+      defaultValue="createdAt"
+      onValueChange={(value) => handleChange(value)}
+    >
+      <SelectTrigger className="no-focus gap-2 border-gray-200 w-[150px]">
+        <SelectValue placeholder="Sort By" />
+      </SelectTrigger>
+
+      <SelectContent>
+        {options.map((option) => (
+          <SelectItem
+            key={`data-sort-by-${option}`}
+            value={option}
+            className="capitalize"
+          >
+            {option}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default DataSortBySelect;
