3cba9338104043f05627869ba2fff3f5bde0e54a
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-26 21:35:36 +0600

    feat: Add DataSortOrderSelect component for selecting data sort order

---
+++ src/components/admin/shared/filters/DataSortOrderSelect.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+const DataSortOrderSelect = () => {
+  const router = useRouter();
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+
+  const params = new URLSearchParams(searchParams);
+
+  const handleChange = (value: string) => {
+    params.delete("sortOrder");
+    params.delete("page");
+    params.append("page", "1");
+    params.append("sortOrder", value);
+
+    router.replace(`${pathName}?${params}`);
+  };
+  return (
+    <Select defaultValue="desc" onValueChange={(value) => handleChange(value)}>
+      <SelectTrigger className="no-focus gap-2 border-gray-200 w-[90px]">
+        <SelectValue placeholder="Sort By" />
+      </SelectTrigger>
+
+      <SelectContent>
+        <SelectItem value={"asc"}>Asc</SelectItem>
+        <SelectItem value={"desc"}>Desc</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default DataSortOrderSelect;
