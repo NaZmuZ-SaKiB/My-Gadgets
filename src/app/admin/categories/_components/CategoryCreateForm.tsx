7e48af586d618fe0d63680667f71bbbe0c01ae7f
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-26 12:00:15 +0600

    feat: Implement category creation functionality in CategoryCreateForm

--- src/app/admin/categories/_components/CategoryCreateForm.tsx
+++ src/app/admin/categories/_components/CategoryCreateForm.tsx
@@ -4,10 +4,13 @@ import AFloatingBox from "@/components/admin/admin-ui/AFloatingBox";
 import MGAInput from "@/components/admin/forms/MGAInput";
 import MGForm from "@/components/global/forms/MGForm";
 import MGButton from "@/components/global/shared/MGButton";
+import { AQTags } from "@/constants/tags";
+import { useCategoryCreateMutation } from "@/lib/queries/category.query";
 import { CategoryValidation } from "@/lib/validations/category.validation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useQueryClient } from "@tanstack/react-query";
 import { SubmitHandler } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
 const defaultValues = {
@@ -18,10 +21,30 @@ const defaultValues = {
 const CategoryCreateForm = () => {
   const queryClient = useQueryClient();
 
+  const { mutateAsync: createCategoryFn, isPending } =
+    useCategoryCreateMutation();
+
   const handleCategoryCreate: SubmitHandler<
     z.infer<typeof CategoryValidation.create>
   > = async (values) => {
-    console.log(values);
+    try {
+      const result = await createCategoryFn({
+        name: values.name.toLowerCase(),
+        label: values.label,
+      });
+
+      if (result?.success) {
+        toast.success(result?.message);
+        queryClient.invalidateQueries({
+          queryKey: [AQTags.CATEGORY, AQTags.ALL],
+          exact: false,
+        });
+      } else {
+        toast.error(result?.message || "A server error occurred.");
+      }
+    } catch (error: any) {
+      toast.error(error?.message || "A client error occurred.");
+    }
   };
   return (
     <AFloatingBox>
@@ -39,8 +62,11 @@ const CategoryCreateForm = () => {
           description="This will be visible in the menu"
         />
 
-        <MGButton className="rounded-none self-start px-5 py-2 h-auto">
-          Create
+        <MGButton
+          className="rounded-none self-start px-5 py-2 h-auto"
+          disabled={isPending}
+        >
+          {isPending ? "Creating..." : "Create Category"}
         </MGButton>
       </MGForm>
     </AFloatingBox>
