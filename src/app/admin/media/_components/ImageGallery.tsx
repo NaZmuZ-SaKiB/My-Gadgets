e70b6c462f2f16feda5037733ba975ac2a1142bb
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-28 11:45:58 +0600

    feat: Add ImageGallery component for displaying images in a grid

---
+++ src/app/admin/media/_components/ImageGallery.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import AFloatingBox from "@/components/admin/admin-ui/AFloatingBox";
+import { useMediaGetAllQuery } from "@/lib/queries/media.query";
+import { TMedia } from "@/types/media.type";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+import UploadImageButton from "./UploadImageButton";
+
+const ImageGallery = () => {
+  const params = new URLSearchParams();
+  params.append("limit", "30");
+
+  const { data, isLoading } = useMediaGetAllQuery(params.toString());
+
+  if (isLoading) {
+    return (
+      <AFloatingBox className="grid place-items-center h-[400px]">
+        <Loader2 className="animate-spin mx-auto size-[50px] text-primary" />
+      </AFloatingBox>
+    );
+  }
+  return (
+    <AFloatingBox>
+      <div className="flex gap-3 flex-wrap">
+        <UploadImageButton />
+        {data?.data?.map((image: TMedia) => (
+          <div
+            key={image._id}
+            className="aspect-square border border-slate-300 bg-slate-100 rounded-lg overflow-hidden cursor-pointer size-[128px]"
+          >
+            <Image
+              src={image.secureUrl}
+              width={128}
+              height={128}
+              alt={image.name}
+              className="size-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
+    </AFloatingBox>
+  );
+};
+
+export default ImageGallery;
