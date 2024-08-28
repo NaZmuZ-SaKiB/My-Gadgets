d7f8308c5080f087deae292ebaf3f349522f7a50
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-28 21:39:42 +0600

    feat: Add BannerSection component for displaying a banner carousel

---
+++ src/app/(root)/_components/BannerSection.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { useRef } from "react";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import { images } from "@/constants";
+import Image from "next/image";
+
+const BannerSection = () => {
+  const plugin = useRef(
+    Autoplay({
+      delay: 5000,
+      stopOnInteraction: false,
+      stopOnFocusIn: false,
+      stopOnMouseEnter: false,
+      stopOnLastSnap: false,
+    })
+  );
+
+  const sliderImages = [images.defaultSliderImage1, images.defaultSliderImage2];
+
+  return (
+    <section className="pt-4 md:pt-8 pb-2 sm:pb-4">
+      <Carousel
+        opts={{
+          loop: true,
+        }}
+        plugins={[plugin.current]}
+        className="rounded-2xl overflow-hidden"
+      >
+        {/* -ml-0 because used pl-0 in item */}
+        <CarouselContent className="-ml-0">
+          {sliderImages.map((image, i) => (
+            <CarouselItem
+              key={`slider-img-${i}`}
+              className="pl-0 relative w-full h-[480px]"
+            >
+              <Image src={image.src} alt={`slider-${i}`} fill className="" />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="left-3 bg-opacity-70 border-gray-300 backdrop-blur-sm" />
+        <CarouselNext className="right-3 bg-opacity-70 border-gray-300 backdrop-blur-sm" />
+      </Carousel>
+    </section>
+  );
+};
+
+export default BannerSection;
