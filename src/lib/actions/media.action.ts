91880fa6cac19f1803c3d02e3a158e70aca5618c
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-28 11:45:22 +0600

    refactor: Add media action functions for creating, updating, and retrieving media objects

---
+++ src/lib/actions/media.action.ts
@@ -0,0 +1,95 @@
+"use server";
+
+import { authKey, backendUrl } from "@/constants";
+import { TMedia } from "@/types/media.type";
+import { cookies } from "next/headers";
+
+export const mediaCreateAction = async (payload: Partial<TMedia>) => {
+  const response = await fetch(`${backendUrl}/api/media`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: cookies().get(authKey)?.value || "",
+    },
+    body: JSON.stringify(payload),
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  return result;
+};
+
+export const mediaUpdateAction = async ({
+  id,
+  payload,
+}: {
+  id: string;
+  payload: Partial<TMedia>;
+}) => {
+  const response = await fetch(`${backendUrl}/api/media/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: cookies().get(authKey)?.value || "",
+    },
+    body: JSON.stringify(payload),
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  return result;
+};
+
+export const mediaGetAllAction = async (params: string) => {
+  const response = await fetch(`${backendUrl}/api/media?${params}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: cookies().get(authKey)?.value || "",
+    },
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  if (!result?.success) {
+    return {
+      data: [],
+      meta: {},
+    };
+  }
+
+  return result;
+};
+
+export const mediaGetByIdAction = async (id: string) => {
+  const response = await fetch(`${backendUrl}/api/media/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  return result;
+};
+
+export const mediaRemoveAction = async (ids: string[]) => {
+  const response = await fetch(`${backendUrl}/api/media`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: cookies().get(authKey)?.value || "",
+    },
+    body: JSON.stringify({ ids }),
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  return result;
+};
