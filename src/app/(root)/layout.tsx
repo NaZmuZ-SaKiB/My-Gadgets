39ef56c1f4f5d92cf5753c805e1570e7e708bcdd
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-28 19:41:14 +0600

    feat: Add UserLayout component to display site header and children

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/global/layout/header/Header";
+
+const UserLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+};
+
+export default UserLayout;
