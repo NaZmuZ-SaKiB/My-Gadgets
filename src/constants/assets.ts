e549c4112eb3db5d67be79476522585b90384782
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-28 21:39:24 +0600

    chore: Add dummy banner and slider images

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -2,6 +2,12 @@
 import DefaultImage from "@/assets/images/default-image.svg";
 import Logo from "@/assets/images/logo.webp";
 import LogoWhite from "@/assets/images/logo-white.webp";
+import DefaultSliderImage1 from "@/assets/images/slider1.webp";
+import DefaultSliderImage2 from "@/assets/images/slider2.webp";
+import DefaultBanner1 from "@/assets/images/banner1.jpg";
+import DefaultBanner2 from "@/assets/images/banner2.jpg";
+import DefaultBanner3 from "@/assets/images/banner3.jpg";
+import DefaultBanner4 from "@/assets/images/banner4.jpg";
 
 // Import icons
 import CartIcon from "@/assets/icons/icon-cart.svg";
@@ -13,6 +19,13 @@ export const images = {
   defaultImage: DefaultImage,
   logo: Logo,
   logoWhite: LogoWhite,
+
+  defaultSliderImage1: DefaultSliderImage1,
+  defaultSliderImage2: DefaultSliderImage2,
+  defaultBanner1: DefaultBanner1,
+  defaultBanner2: DefaultBanner2,
+  defaultBanner3: DefaultBanner3,
+  defaultBanner4: DefaultBanner4,
 };
 
 export const icons = {
