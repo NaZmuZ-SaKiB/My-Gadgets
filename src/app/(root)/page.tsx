9a05ff23b7c18be56eef4ea71b9d6b74623e0d3a
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-28 22:19:26 +0600

    feat: Add FeaturedCategories component to homepage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import BannerSection from "./_components/BannerSection";
+import FeaturedCategories from "./_components/FeaturedCategories";
 
 const HomePage = () => {
   return (
     <div className="mg-container">
       <BannerSection />
+      <FeaturedCategories />
     </div>
   );
 };
