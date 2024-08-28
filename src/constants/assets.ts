7bd6886d5489071c64b8a8100a45cc9064a9500e
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-28 15:57:39 +0600

    feat: Add defaultImage to images constant

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -1,7 +1,9 @@
+import DefaultImage from "@/assets/images/default-image.svg";
 import Logo from "@/assets/images/logo.webp";
 import LogoWhite from "@/assets/images/logo-white.webp";
 
 export const images = {
+  defaultImage: DefaultImage,
   logo: Logo,
   logoWhite: LogoWhite,
 };
