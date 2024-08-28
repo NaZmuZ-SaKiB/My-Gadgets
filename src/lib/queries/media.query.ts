a7d331d309689edb752b53b60c808d65bd02cd27
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-28 11:45:32 +0600

    refactor: Add media.query.ts for handling media-related queries and mutations

---
+++ src/lib/queries/media.query.ts
@@ -0,0 +1,36 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+  mediaCreateAction,
+  mediaGetAllAction,
+  mediaGetByIdAction,
+  mediaRemoveAction,
+  mediaUpdateAction,
+} from "../actions/media.action";
+import { AQTags } from "@/constants/tags";
+
+export const useMediaCreateMutation = () =>
+  useMutation({
+    mutationFn: mediaCreateAction,
+  });
+
+export const useMediaUpdateMutation = () =>
+  useMutation({
+    mutationFn: mediaUpdateAction,
+  });
+
+export const useMediaGetAllQuery = (params: string) =>
+  useQuery({
+    queryKey: [AQTags.MEDIA, AQTags.ALL, params],
+    queryFn: () => mediaGetAllAction(params),
+  });
+
+export const useMediaGetByIdQuery = (id: string) =>
+  useQuery({
+    queryKey: [AQTags.MEDIA, id],
+    queryFn: () => mediaGetByIdAction(id),
+  });
+
+export const useMediaDeleteMutation = () =>
+  useMutation({
+    mutationFn: mediaRemoveAction,
+  });
