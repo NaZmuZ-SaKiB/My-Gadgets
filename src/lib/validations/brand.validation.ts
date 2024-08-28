483501fbd14d43fe6f8d1a94f834d98864b8886a
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-28 13:06:25 +0600

    feat: Add optional image field to brand validation

--- src/lib/validations/brand.validation.ts
+++ src/lib/validations/brand.validation.ts
@@ -8,6 +8,11 @@ export const create = z.object({
     })
     .min(3, "Name must be at least 3 characters long")
     .max(20, "Name must be at most 20 characters long"),
+  image: z
+    .string({
+      invalid_type_error: "Image must be a string",
+    })
+    .optional(),
 });
 
 export const BrandValidation = {
