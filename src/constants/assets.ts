76cc04f2ecdec95c8198b123c50a02fd67c3e454
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-28 17:39:26 +0600

    feat: Add icons and import images and icons in assets.ts

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -1,9 +1,23 @@
+// Import images
 import DefaultImage from "@/assets/images/default-image.svg";
 import Logo from "@/assets/images/logo.webp";
 import LogoWhite from "@/assets/images/logo-white.webp";
+
+// Import icons
+import CartIcon from "@/assets/icons/icon-cart.svg";
+import HeartIcon from "@/assets/icons/icon-heart.svg";
+import CompareIcon from "@/assets/icons/icon-compare.svg";
+import UserIcon from "@/assets/icons/icon-user.svg";
 
 export const images = {
   defaultImage: DefaultImage,
   logo: Logo,
   logoWhite: LogoWhite,
+};
+
+export const icons = {
+  cart: CartIcon,
+  heart: HeartIcon,
+  compare: CompareIcon,
+  user: UserIcon,
 };
