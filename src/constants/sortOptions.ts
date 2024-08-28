f1d5943bccce47ca32f90454209ac63120a79ba9
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-28 11:45:13 +0600

    refactor: Add mediaSortOptions constant for sorting media objects

--- src/constants/sortOptions.ts
+++ src/constants/sortOptions.ts
@@ -1,3 +1,5 @@
 export const categorySortOptions = ["name", "label", "createdAt", "updatedAt"];
 
 export const brandSortOptions = ["name", "createdAt", "updatedAt"];
+
+export const mediaSortOptions = ["createdAt", "updatedAt"];
