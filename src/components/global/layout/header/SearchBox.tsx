2981b69e929b09037695bb1b0d2098ce45a20533
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-28 17:40:08 +0600

    feat: Add SearchBox component to header for displaying search functionality

---
+++ src/components/global/layout/header/SearchBox.tsx
@@ -0,0 +1,18 @@
+import { Search } from "lucide-react";
+
+const SearchBox = () => {
+  return (
+    <div className="xl:search-box-pc search-box max-xl:search-box-mobile bg-white">
+      <input
+        className="flex-1 focus:outline-none"
+        type="text"
+        autoComplete="off"
+      />
+      <button className="flex shrink-0 items-center justify-center">
+        <Search className="h-6 text-primary-1" />
+      </button>
+    </div>
+  );
+};
+
+export default SearchBox;
