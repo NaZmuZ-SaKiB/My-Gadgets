676b256f2b34a96d84b4cad135d902c646de4ac9
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-28 11:45:51 +0600

    refactor: Add UploadImageButton component for handling image uploads

---
+++ src/app/admin/media/_components/UploadImageButton.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import MGButton from "@/components/global/shared/MGButton";
+import { AQTags } from "@/constants/tags";
+import { useMediaCreateMutation } from "@/lib/queries/media.query";
+import { useQueryClient } from "@tanstack/react-query";
+import { Plus } from "lucide-react";
+import { CldUploadWidget } from "next-cloudinary";
+import { toast } from "sonner";
+
+const UploadImageButton = () => {
+  const queryClient = useQueryClient();
+
+  const { mutateAsync: createImageFn } = useMediaCreateMutation();
+
+  const handleSuccess = async (result: any) => {
+    const { info } = result;
+
+    try {
+      await createImageFn({
+        name: info.display_name,
+        publicId: info.public_id,
+        height: info.height,
+        width: info.width,
+        format: info.format,
+        url: info.url,
+        secureUrl: info.secure_url,
+        thumbnailUrl: info.thumbnail_url,
+      });
+    } catch (error: any) {
+      toast.error(error?.message || "A client error occurred.");
+    }
+  };
+
+  const onClose = () => {
+    queryClient.invalidateQueries({
+      queryKey: [AQTags.MEDIA, AQTags.ALL],
+      exact: false,
+    });
+  };
+
+  return (
+    <CldUploadWidget
+      options={{
+        folder: process.env.NEXT_PUBLIC_CLOUDINARY_FOLDER_NAME,
+        sources: ["local", "url", "google_drive", "image_search"],
+        maxFileSize: 1024 * 1024,
+        multiple: true,
+        showCompletedButton: true,
+      }}
+      signatureEndpoint={"/api/sign-image"}
+      onSuccess={handleSuccess}
+      onClose={onClose}
+    >
+      {({ open }) => (
+        <div className="size-[128px] border border-gray-300 text-gray-300 rounded-lg hover:border-primary hover:text-primary cursor-pointer">
+          <Plus className="size-full" onClick={() => open()} />
+        </div>
+      )}
+    </CldUploadWidget>
+  );
+};
+
+export default UploadImageButton;
