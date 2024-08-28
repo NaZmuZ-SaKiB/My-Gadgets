96e639f0020d0ccac5106d2d8c362ef5f3e87a5d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-28 22:19:04 +0600

    feat: Add FeaturedCategories component for displaying featured categories on the homepage

---
+++ src/app/(root)/_components/FeaturedCategories.tsx
@@ -0,0 +1,62 @@
+import { categoryGetAllAction } from "@/lib/actions/category.action";
+import { TCategory } from "@/types/category.type";
+import Image from "next/image";
+
+const FeaturedCategories = async () => {
+  const params = new URLSearchParams();
+  params.append("featured", "true");
+  params.append("limit", "12");
+
+  const categoryData = await categoryGetAllAction(params.toString());
+  const categories: TCategory[] = categoryData.data || [];
+
+  const colors: string[] = [
+    "#fffceb",
+    "#ecffec",
+    "#feefea",
+    "#fff3eb",
+    "#fff3ff",
+    "#f2fce4",
+    "#feefea",
+    "#fffceb",
+    "#feefea",
+    "#ecffec",
+    "#feefea",
+    "#fff3eb",
+    "#fff3ff",
+    "#f2fce4",
+    "#feefea",
+    "#fffceb",
+    "#feefea",
+    "#ecffec",
+  ];
+
+  return (
+    <section className="md:pt-6 pb-4">
+      <h2 className="font-bold text-3xl text-gray-700">Featured Categories</h2>
+
+      <div className="flex flex-wrap gap-3 md:gap-4 mt-8">
+        {categories.map((category, i) => (
+          <div
+            key={`featured-category-${category._id}`}
+            className={`rounded-xl flex flex-col justify-center items-center p-4 gap-4 flex-1 basis-32 sm:basis-40 hover:shadow-lg transition-shadow duration-500 cursor-pointer`}
+            style={{ backgroundColor: colors[i] }}
+          >
+            <Image
+              src={category.image!.secureUrl}
+              alt={category.name}
+              height={64}
+              width={64}
+              className="object-contain"
+            />
+            <span className="font-bold text-gray-700 text-center max-sm:text-sm">
+              {category.name}
+            </span>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default FeaturedCategories;
