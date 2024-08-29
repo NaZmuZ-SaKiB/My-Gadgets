f5a72ba42fd2245e806a72434645b02964d0981a
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-29 12:47:55 +0600

    refactor: Update FeaturedCategories component to improve styling and layout

--- src/app/(root)/_components/FeaturedCategories.tsx
+++ src/app/(root)/_components/FeaturedCategories.tsx
@@ -14,14 +14,14 @@ const FeaturedCategories = async () => {
     "#fffceb",
     "#ecffec",
     "#feefea",
-    "#fff3eb",
     "#fff3ff",
+    "#fff3eb",
     "#f2fce4",
     "#feefea",
     "#fffceb",
-    "#feefea",
     "#ecffec",
     "#feefea",
+    "#f2fce4",
     "#fff3eb",
     "#fff3ff",
     "#f2fce4",
@@ -32,14 +32,16 @@ const FeaturedCategories = async () => {
   ];
 
   return (
-    <section className="md:pt-6 pb-4">
-      <h2 className="font-bold text-3xl text-gray-700">Featured Categories</h2>
+    <section className="pt-4 md:pt-6 pb-4">
+      <h2 className="font-bold text-2xl md:text-3xl text-gray-700">
+        Featured Categories
+      </h2>
 
-      <div className="flex flex-wrap gap-3 md:gap-4 mt-8">
+      <div className="flex flex-wrap gap-3 md:gap-4 mt-5 md:mt-8">
         {categories.map((category, i) => (
           <div
             key={`featured-category-${category._id}`}
-            className={`rounded-xl flex flex-col justify-center items-center p-4 gap-4 flex-1 basis-32 sm:basis-40 hover:shadow-lg transition-shadow duration-500 cursor-pointer`}
+            className={`rounded-xl flex flex-col justify-center items-center p-4 gap-4 flex-1 basis-32 sm:basis-48 hover:shadow-lg transition-shadow duration-500 cursor-pointer`}
             style={{ backgroundColor: colors[i] }}
           >
             <Image
