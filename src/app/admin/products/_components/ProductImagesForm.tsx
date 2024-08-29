3739a2e3e7b8f01d3c0834e3a7cdedd46f6c6b65
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-29 21:34:30 +0600

    feat: Add ProductImagesForm component for managing product images

---
+++ src/app/admin/products/_components/ProductImagesForm.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import AFloatingBox from "@/components/admin/admin-ui/AFloatingBox";
+import MGAImageInput from "@/components/admin/forms/MGAImageInput";
+import { TProduct } from "@/types/product.type";
+
+const ProductImagesForm = ({ defaultValues }: { defaultValues?: TProduct }) => {
+  return (
+    <AFloatingBox>
+      <h2 className="font-medium text-gray-700 text-lg mb-5">Images</h2>
+
+      <div className="space-y-3">
+        <MGAImageInput
+          name="images"
+          label="Gallery Images*"
+          description="First selected image will be the featured image."
+          defaultValue={defaultValues?.images || []}
+          multiple
+        />
+      </div>
+    </AFloatingBox>
+  );
+};
+
+export default ProductImagesForm;
