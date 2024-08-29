282fdbd86b70e10654d1d9090c3699e61aae32ab
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-29 22:30:21 +0600

    refactor: Update productSortOptions in sortOptions.ts

--- src/constants/sortOptions.ts
+++ src/constants/sortOptions.ts
@@ -3,3 +3,10 @@ export const categorySortOptions = ["name", "label", "createdAt", "updatedAt"];
 export const brandSortOptions = ["name", "createdAt", "updatedAt"];
 
 export const mediaSortOptions = ["createdAt", "updatedAt"];
+
+export const productSortOptions = [
+  "name",
+  "salePrice",
+  "createdAt",
+  "updatedAt",
+];
