4f05f2b7b2a0842e88f2d1f91c33d32ccd900b9b
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-29 19:31:57 +0600

    feat: Add ProductFiltersForm component for filtering products

---
+++ src/app/admin/products/_components/ProductFiltersForm.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import AFloatingBox from "@/components/admin/admin-ui/AFloatingBox";
+import MGAInput from "@/components/admin/forms/MGAInput";
+import MGAMultiSelect from "@/components/admin/forms/MGAMultiSelect";
+import MGASelect from "@/components/admin/forms/MGASelect";
+import {
+  chargingPortOptions,
+  compatibilityOptions,
+  connectivityOptions,
+  operatingSystemOptions,
+  powerSourceOptions,
+} from "@/constants/product";
+
+const ProductFiltersForm = () => {
+  return (
+    <AFloatingBox className="">
+      <div className="max-w-md flex flex-col gap-3">
+        <MGAMultiSelect
+          name="compatibility"
+          label="Compatibility"
+          options={compatibilityOptions}
+          vertical
+        />
+
+        <MGAMultiSelect
+          name="connectivity"
+          label="Connectivity"
+          options={connectivityOptions}
+          vertical
+        />
+
+        <MGAMultiSelect
+          name="powerSource"
+          label="Power Source"
+          options={powerSourceOptions}
+          vertical
+        />
+
+        <MGASelect
+          name="operatingSystem"
+          label="Operating System"
+          options={operatingSystemOptions}
+          vertical
+        />
+
+        <MGASelect
+          name="chargingPort"
+          label="Charging Port"
+          options={chargingPortOptions}
+          vertical
+        />
+
+        <MGAInput
+          name="weight"
+          label="Weight"
+          type="number"
+          description="In Kg unit. Ex: 1.25"
+          vertical
+        />
+        <MGAInput name="camera" label="Camera" type="number" vertical />
+        <MGAInput
+          name="displaySize"
+          label="Display Size"
+          type="number"
+          description="In inches. Ex: 15.6"
+          vertical
+        />
+      </div>
+    </AFloatingBox>
+  );
+};
+
+export default ProductFiltersForm;
