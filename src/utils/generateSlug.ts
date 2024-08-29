dd5904dc31a55ffba686712c08ea6e9847e70cdd
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-29 17:32:23 +0600

    feat: Add generateSlug utility function

---
+++ src/utils/generateSlug.ts
@@ -0,0 +1,10 @@
+const generateSlug = (text: string) => {
+  return text
+    .toLowerCase()
+    .replace(/[^a-z0-9\s-]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-")
+    .replace(/^-+|-+$/g, "");
+};
+
+export default generateSlug;
