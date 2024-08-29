00584b1c3de956140e43122595b5516a1af3c324
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-29 16:25:52 +0600

    feat: Add product query hooks for CRUD operations

---
+++ src/lib/queries/product.query.ts
@@ -0,0 +1,36 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+  productCreateAction,
+  productGetAllAction,
+  productGetByIdAction,
+  productRemoveAction,
+  productUpdateAction,
+} from "../actions/product.action";
+import { AQTags } from "@/constants/tags";
+
+export const useProductCreateMutation = () =>
+  useMutation({
+    mutationFn: productCreateAction,
+  });
+
+export const useProductUpdateMutation = () =>
+  useMutation({
+    mutationFn: productUpdateAction,
+  });
+
+export const useProductGetAllQuery = (params: string) =>
+  useQuery({
+    queryKey: [AQTags.PRODUCT, AQTags.ALL, params],
+    queryFn: () => productGetAllAction(params),
+  });
+
+export const useProductGetByIdQuery = (id: string) =>
+  useQuery({
+    queryKey: [AQTags.PRODUCT, id],
+    queryFn: () => productGetByIdAction(id),
+  });
+
+export const useProductDeleteMutation = () =>
+  useMutation({
+    mutationFn: productRemoveAction,
+  });
