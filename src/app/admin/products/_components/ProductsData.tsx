8d3b10ff05c2fc1238e49c790ad11e4f60717600
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-29 22:30:11 +0600

    refactor: Add ProductsData component for managing product data in admin panel

---
+++ src/app/admin/products/_components/ProductsData.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import AFloatingBox from "@/components/admin/admin-ui/AFloatingBox";
+import DataLimitSelect from "@/components/admin/shared/filters/DataLimitSelect";
+import DataSearchBox from "@/components/admin/shared/filters/DataSearchBox";
+import DataSortBySelect from "@/components/admin/shared/filters/DataSortBySelect";
+import DataSortOrderSelect from "@/components/admin/shared/filters/DataSortOrderSelect";
+import SelectedItemsCount from "@/components/admin/shared/SelectedItemsCount";
+import { productSortOptions } from "@/constants";
+import ProductsDataTable from "./ProductsDataTable";
+import { useState } from "react";
+
+const ProductsData = () => {
+  const [selectedProducts, setSelectedProducts] = useState<string[]>([]);
+  return (
+    <div className="flex flex-col gap-3 h-full">
+      <AFloatingBox className="flex flex-col gap-2">
+        <div className="flex gap-2 flex-wrap">
+          <SelectedItemsCount count={selectedProducts.length} />
+
+          {/* Product Delete Dialog  */}
+
+          <div className="sm:hidden">
+            <DataLimitSelect />
+          </div>
+
+          <DataSortBySelect options={productSortOptions} />
+
+          <DataSortOrderSelect />
+        </div>
+
+        <DataSearchBox />
+      </AFloatingBox>
+
+      <ProductsDataTable
+        selectedProducts={selectedProducts}
+        setSelectedProducts={setSelectedProducts}
+      />
+    </div>
+  );
+};
+
+export default ProductsData;
