b53894dc6c6c2eef48051181b337ff6bf09a19eb
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-29 12:48:04 +0600

    refactor: Update ATopbar component to include a link to the homepage

--- src/components/admin/layout/ATopbar.tsx
+++ src/components/admin/layout/ATopbar.tsx
@@ -1,18 +1,21 @@
 import { images } from "@/constants";
 import Image from "next/image";
 import ASidebarDrawer from "./ASidebarDrawer";
+import Link from "next/link";
 
 const ATopbar = () => {
   return (
     <div className="px-4 py-3 flex gap-5 items-center justify-between bg-slate-800">
       <div className="flex gap-3 items-center">
         <ASidebarDrawer />
-        <Image
-          src={images.logoWhite.src}
-          alt="My Gadgets"
-          width={35}
-          height={35}
-        />
+        <Link href="/">
+          <Image
+            src={images.logoWhite.src}
+            alt="My Gadgets"
+            width={35}
+            height={35}
+          />
+        </Link>
       </div>
     </div>
   );
