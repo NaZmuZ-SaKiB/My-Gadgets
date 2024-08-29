af54c74b9ea61d4494da653011afbe34bea7fef3
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-29 19:32:13 +0600

    refactor: Update product constants for compatibility options

---
+++ src/constants/product.ts
@@ -0,0 +1,58 @@
+import {
+  TChargingPort,
+  TCompatibility,
+  TConnectivity,
+  TOperatingSystem,
+  TPowerSource,
+} from "@/types/product.type";
+
+export const operatingSystems: TOperatingSystem[] = [
+  "android",
+  "ios",
+  "linux",
+  "macos",
+  "windows",
+];
+
+export const operatingSystemOptions = operatingSystems.map((os) => ({
+  label: os,
+  value: os,
+}));
+
+export const chargingPorts: TChargingPort[] = [
+  "usb-c",
+  "lightning",
+  "micro-usb",
+];
+
+export const chargingPortOptions = chargingPorts.map((port) => ({
+  label: port,
+  value: port,
+}));
+
+export const connectivities: TConnectivity[] = ["bluetooth", "wifi"];
+
+export const connectivityOptions = connectivities.map((connectivity) => ({
+  label: connectivity,
+  value: connectivity,
+}));
+
+export const powerSources: TPowerSource[] = ["battery", "plug-in"];
+
+export const powerSourceOptions = powerSources.map((source) => ({
+  label: source,
+  value: source,
+}));
+
+export const compatibilities: TCompatibility[] = [
+  ...operatingSystems,
+  "iphone",
+  "android-phone",
+  "laptop",
+  "macbook",
+];
+
+export const compatibilityOptions = compatibilities.map((compatibility) => ({
+  label: compatibility,
+  value: compatibility,
+}));
