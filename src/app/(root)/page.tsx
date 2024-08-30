add81bfeb7a9b3a392e4c282a8625b4008f59b73
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-30 17:58:18 +0600

    feat: Add PopularProducts component and update HomePage to include it

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,13 @@
 import BannerSection from "./_components/BannerSection";
 import FeaturedCategories from "./_components/FeaturedCategories";
+import PopularProducts from "./_components/PopularProducts";
 
 const HomePage = () => {
   return (
     <div className="mg-container">
       <BannerSection />
       <FeaturedCategories />
+      <PopularProducts />
     </div>
   );
 };
