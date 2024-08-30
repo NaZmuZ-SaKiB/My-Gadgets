efbb13ba6e2ada6636f93a448aceea8d92f6162f
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-30 11:38:05 +0600

    refactor: Add "Add Product" button to ProductsPage

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,11 +1,20 @@
 import APageContainer from "@/components/admin/admin-ui/APageContainer";
 import APageHeading from "@/components/admin/admin-ui/APageHeading";
 import ProductsData from "./_components/ProductsData";
+import Link from "next/link";
+import MGButton from "@/components/global/shared/MGButton";
+import { Plus } from "lucide-react";
 
 const ProductsPage = () => {
   return (
     <APageContainer>
-      <APageHeading title="Products" />
+      <APageHeading title="Products">
+        <Link href={"/admin/products/add-product"}>
+          <MGButton className="rounded-none self-start px-5 py-2 h-auto">
+            <Plus className="mr-2" /> Add Product
+          </MGButton>
+        </Link>
+      </APageHeading>
       <ProductsData />
     </APageContainer>
   );
