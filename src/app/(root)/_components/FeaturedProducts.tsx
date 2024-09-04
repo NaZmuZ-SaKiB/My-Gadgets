266a5227aa8474627523e553baacb43c2059823d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-04 15:02:03 +0600

    feat: Update FeaturedProducts component to use props for featured products

--- src/app/(root)/_components/FeaturedProducts.tsx
+++ src/app/(root)/_components/FeaturedProducts.tsx
@@ -1,31 +1,41 @@
 import ProductSlider from "@/components/global/shared/ProductSlider";
 import { images } from "@/constants";
 import { productGetAllAction } from "@/lib/actions/product.action";
+import { TMedia } from "@/types/media.type";
 import { TProduct } from "@/types/product.type";
 import Image from "next/image";
 
-const FeaturedProducts = async () => {
-  const params = new URLSearchParams();
-  params.append("category", "gaming");
+type TProps = {
+  featuredProducts: { banner?: TMedia; products: TProduct[] }[];
+};
 
-  const productsData = await productGetAllAction(params.toString());
-  const products: TProduct[] = productsData.data || [];
+const FeaturedProducts = ({ featuredProducts }: TProps) => {
+  // const params = new URLSearchParams();
+  // params.append("category", "gaming");
 
+  // const productsData = await productGetAllAction(params.toString());
+  // const products: TProduct[] = productsData.data || [];
+  console.log(featuredProducts[0].banner);
   return (
     <section className="pt-4 md:pt-6 pb-4">
       <h2 className="font-bold text-3xl text-slate-700">Featured Products</h2>
 
-      <div className="flex gap-4 mt-8">
-        <div className="shrink-0 max-md:hidden">
-          <Image
-            src={images.defaultBanner4}
-            alt="banner"
-            className="object-cover object-center w-72 h-full rounded-2xl"
-          />
-        </div>
+      {featuredProducts.map((item, index) => (
+        <div className="flex gap-4 mt-8" key={`featured-products-${index}`}>
+          {item.banner && (
+            <div className="shrink-0 max-md:hidden relative basis-[280px]">
+              <Image
+                src={item.banner.secureUrl}
+                alt="banner"
+                className="object-cover object-center w-72 h-full rounded-2xl"
+                fill
+              />
+            </div>
+          )}
 
-        <ProductSlider products={products} withBanner />
-      </div>
+          <ProductSlider products={item.products} withBanner />
+        </div>
+      ))}
     </section>
   );
 };
