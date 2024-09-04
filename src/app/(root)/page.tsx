5dbf22500bd8a50beabb98c0bafa88cd6f989585
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-04 14:23:34 +0600

    feat: Update PopularProducts component to pass popular products as props

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -24,7 +24,7 @@ const HomePage = async () => {
       <FeaturedCategories
         featuredCategories={homePageSettings.featuredCategories || []}
       />
-      <PopularProducts />
+      <PopularProducts popularProducts={homePageSettings.popularProducts} />
       <FeaturedProducts />
       <FlashSaleSection />
       <ProductListsSection />
