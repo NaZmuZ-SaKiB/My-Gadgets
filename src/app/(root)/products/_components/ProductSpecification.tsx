4bae9bcc645cd21347992d568051170595167801
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-05 22:49:04 +0600

    refactor: Add ProductSpecification component for displaying product specifications on SingleProductPage

---
+++ src/app/(root)/products/_components/ProductSpecification.tsx
@@ -0,0 +1,22 @@
+import { TProduct } from "@/types/product.type";
+
+type TProps = {
+  product: TProduct;
+};
+
+const ProductSpecification = ({ product }: TProps) => {
+  return (
+    <section className="mt-4 p-2 xs:p-6 bg-slate-50 rounded-2xl">
+      <h2 className="text-xl font-bold">Specification</h2>
+
+      <div
+        className="mt-4 specification-table"
+        dangerouslySetInnerHTML={{
+          __html: product.specifications,
+        }}
+      />
+    </section>
+  );
+};
+
+export default ProductSpecification;
