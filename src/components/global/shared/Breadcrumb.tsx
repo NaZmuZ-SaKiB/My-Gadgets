c0d4ed04043327c643a549257a0571659fd82060
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-05 09:27:34 +0600

    feat: Add Breadcrumb component and related UI components

---
+++ src/components/global/shared/Breadcrumb.tsx
@@ -0,0 +1,60 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { cn } from "@/lib/utils";
+import { Home } from "lucide-react";
+import Link from "next/link";
+import { Fragment } from "react/jsx-runtime";
+
+type TProps = {
+  items: {
+    label: string;
+    link?: string;
+  }[];
+};
+
+const BreadcrumbBar = ({ items }: TProps) => {
+  return (
+    <Breadcrumb className="sm:p-4 sm:border sm:border-slate-300 sm:rounded-xl overflow-hidden">
+      <BreadcrumbList className="flex-nowrap">
+        <BreadcrumbItem className="text-primary">
+          <Link href="/">
+            <Home className="size-5" />
+          </Link>
+        </BreadcrumbItem>
+
+        <BreadcrumbSeparator />
+
+        {items.map((item, i) => {
+          const DinamicTag = item.link ? Link : "span";
+          return (
+            <Fragment key={`breadcrumb-item-${item.label}-${i}`}>
+              <BreadcrumbItem
+                className={cn("text-primary font-semibold overflow-hidden", {
+                  "text-slate-500": i === items.length - 1,
+                  "shrink-0": i < items.length - 1,
+                })}
+              >
+                <DinamicTag
+                  href={item?.link ?? ""}
+                  className={cn({
+                    "whitespace-nowrap overflow-hidden text-ellipsis md:max-w-[300px]":
+                      i === items.length - 1,
+                  })}
+                >
+                  {item.label}
+                </DinamicTag>
+              </BreadcrumbItem>
+              {i < items.length - 1 && <BreadcrumbSeparator />}
+            </Fragment>
+          );
+        })}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+export default BreadcrumbBar;
