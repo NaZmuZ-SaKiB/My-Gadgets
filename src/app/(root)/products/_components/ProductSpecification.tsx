4b0f38c7d44581ee30d12e9437f089595652c659
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-06 15:50:02 +0600

    refactor: Update ProductSpecification component to use a more specific CSS class for the title

--- src/app/(root)/products/_components/ProductSpecification.tsx
+++ src/app/(root)/products/_components/ProductSpecification.tsx
@@ -7,7 +7,7 @@ type TProps = {
 const ProductSpecification = ({ product }: TProps) => {
   return (
     <section className="mt-4 p-2 xs:p-6 bg-slate-50 rounded-2xl">
-      <h2 className="text-xl font-bold">Specification</h2>
+      <h2 className="text-xl text-slate-700 font-semibold">Specification</h2>
 
       <div
         className="mt-4 specification-table"
