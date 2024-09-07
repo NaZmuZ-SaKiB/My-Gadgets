8d4393ba7fc510875b5c97734a534d664f7cf009
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-07 17:38:18 +0600

    Refactor review type to include _id field

--- src/types/review.type.ts
+++ src/types/review.type.ts
@@ -4,6 +4,7 @@ import { TUser } from "./user.type";
 export type TReviewStatus = "pending" | "approved" | "rejected";
 
 export type TReview = {
+  _id: string;
   product: TProduct;
   user: TUser;
   rating: number;
