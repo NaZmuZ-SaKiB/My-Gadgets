46aa7578da720d8a144571459ff04fdabb89871d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-07 13:38:14 +0600

    refactor: Update review validation to make rating optional

--- src/lib/validations/review.validation.ts
+++ src/lib/validations/review.validation.ts
@@ -2,9 +2,8 @@ import { z } from "zod";
 
 const create = z.object({
   product: z.string(),
-  rating: z.number().int().min(1).max(5),
   comment: z.string().min(1).max(500),
-  status: z.string(),
+  rating: z.number().int().min(1).max(5).optional(),
 });
 
 export const ReviewValidation = {
