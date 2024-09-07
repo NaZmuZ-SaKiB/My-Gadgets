ad0c448e3bc85ab525bdcdd7e01462b108931834
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-07 13:38:58 +0600

    feat: Add StarRatingComponent

---
+++ src/components/global/shared/StarRatingComponent.tsx
@@ -0,0 +1,57 @@
+import { images } from "@/constants";
+import { Star } from "lucide-react";
+import Image from "next/image";
+
+type TProps = {
+  readonly?: boolean;
+  maxStars?: number;
+  value: number;
+  setValue?: (rating: number) => void;
+};
+
+const StarRatingComponent = ({
+  readonly = false,
+  maxStars = 5,
+  value,
+  setValue = () => {},
+}: TProps) => {
+  if (!readonly) {
+    return (
+      <div className="flex gap-1 items-end">
+        {Array(maxStars)
+          .fill(0)
+          .map((_, i) => (
+            <span
+              key={`rating-star-${i}`}
+              onClick={() => setValue(i + 1)}
+              className="cursor-pointer"
+            >
+              <Star
+                fill={i + 1 <= value ? "orange" : "lightgray"}
+                color={i + 1 <= value ? "orange" : "lightgray"}
+              />
+            </span>
+          ))}
+        <span className="text-lg pl-1">({value})</span>
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className="inline-block"
+        style={{
+          backgroundImage: `linear-gradient(90deg, #FDAF07  ${value}%, #D8D9DB  ${value}%)`,
+          display: "flex",
+        }}
+      >
+        <Image
+          src={images.fullStar}
+          className="inline-block w-full"
+          alt="rating stars"
+        />
+      </div>
+    );
+  }
+};
+
+export default StarRatingComponent;
