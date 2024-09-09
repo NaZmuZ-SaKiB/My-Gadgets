5cbaf770f7ec7afef76cfc896a30c1a1ad03f362
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-09 17:17:33 +0600

    Refactor range-slider component to update label position in SliderPrimitive.Thumb

--- src/components/ui/range-slider.tsx
+++ src/components/ui/range-slider.tsx
@@ -28,17 +28,17 @@ const RangeSlider = React.forwardRef<
       )}
       {...props}
     >
-      <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
+      <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-slate-100">
         <SliderPrimitive.Range className="absolute h-full bg-primary" />
       </SliderPrimitive.Track>
       {initialValue.map((value, index) => (
         <React.Fragment key={index}>
-          <SliderPrimitive.Thumb className="relative block h-4 w-4 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
+          <SliderPrimitive.Thumb className="bg-white relative block h-4 w-4 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
             {label && (
               <span
                 className={cn(
                   "absolute flex w-full justify-center",
-                  labelPosition === "top" && "-top-7",
+                  labelPosition === "top" && "-top-5",
                   labelPosition === "bottom" && "top-4"
                 )}
               >
