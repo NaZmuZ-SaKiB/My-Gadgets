78f65565feb474efcac395bf26f4b7daae43207f
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-09 19:33:30 +0600

    Refactor ShopPage component to include FilterTopBar and display ProductCard components

--- src/app/(root)/shop/[category]/page.tsx
+++ src/app/(root)/shop/[category]/page.tsx
@@ -1,6 +1,9 @@
 import BreadcrumbBar from "@/components/global/shared/Breadcrumb";
 import { productGetAllAction } from "@/lib/actions/product.action";
 import Filters from "../_components/Filters";
+import FilterTopBar from "../_components/FilterTopBar";
+import ProductCard from "@/components/global/cards/ProductCard";
+import { TProduct } from "@/types/product.type";
 
 type TProps = {
   params: {
@@ -11,9 +14,9 @@ type TProps = {
 
 const ShopPage = async ({ params, searchParams }: TProps) => {
   const productsData = await productGetAllAction(
-    `limit=30&category=${params.category}&${searchParams.toString()}`
+    `limit=35&category=${params.category}&${searchParams.toString()}`
   );
-  const products = productsData.data || [];
+  const products: TProduct[] = productsData.data || [];
 
   const breadcrumbItems = [
     {
@@ -26,9 +29,20 @@ const ShopPage = async ({ params, searchParams }: TProps) => {
     <div className="mg-container py-4">
       <BreadcrumbBar items={breadcrumbItems} />
 
-      <div className="lg:grid grid-cols-[250px_1fr] gap-3 mt-5">
+      <div className="lg:grid grid-cols-[250px_1fr] gap-3 mt-3">
         <div className="bg-white max-lg:hidden">
           <Filters />
+        </div>
+        <div>
+          <FilterTopBar />
+
+          <div className="mt-3 mg-shop-grid">
+            {products.map((product) => (
+              <div key={`shop-product-${product._id}`} className="">
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
