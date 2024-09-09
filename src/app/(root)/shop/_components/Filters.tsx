8a9d8b575c25ea24b8f04aa3872e02fe6f5796cd
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-09 17:17:46 +0600

    Refactor Filters component to include PriceFilter

---
+++ src/app/(root)/shop/_components/Filters.tsx
@@ -0,0 +1,11 @@
+import PriceFilter from "./PriceFilter";
+
+const Filters = () => {
+  return (
+    <div className="flex flex-col gap-3">
+      <PriceFilter />
+    </div>
+  );
+};
+
+export default Filters;
