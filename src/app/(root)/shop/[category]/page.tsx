a0a5a7490555dc960cbe07747333d776fab1f155
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-09 16:52:10 +0600

    Refactor ShopPage component to remove console.log statements

---
+++ src/app/(root)/shop/[category]/page.tsx
@@ -0,0 +1,35 @@
+import BreadcrumbBar from "@/components/global/shared/Breadcrumb";
+import { productGetAllAction } from "@/lib/actions/product.action";
+
+type TProps = {
+  params: {
+    category: string;
+  };
+  searchParams: any;
+};
+
+const ShopPage = async ({ params, searchParams }: TProps) => {
+  const productsData = await productGetAllAction(
+    `limit=30&category=${params.category}&${searchParams.toString()}`
+  );
+  const products = productsData.data || [];
+
+  const breadcrumbItems = [
+    {
+      label: params.category as string,
+      link: "",
+    },
+  ];
+
+  return (
+    <div className="mg-container py-4">
+      <BreadcrumbBar items={breadcrumbItems} />
+
+      <div className="lg:grid grid-cols-[250px_1fr] gap-3 mt-5">
+        <div className="bg-white max-lg:hidden"></div>
+      </div>
+    </div>
+  );
+};
+
+export default ShopPage;
