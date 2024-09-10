a07d41aa8fc6520a92d922fd6c1da235027e7b0a
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-10 15:54:50 +0600

    Refactor PriceFilter component to update border color and text style

--- src/app/(root)/shop/_components/PriceFilter.tsx
+++ src/app/(root)/shop/_components/PriceFilter.tsx
@@ -7,14 +7,14 @@ const PriceFilter = () => {
   const [values, setValues] = useState<number[]>([0, 300000]);
 
   return (
-    <div className="rounded-xl border border-slate-300">
-      <h3 className="font-bold text-lg px-3 py-2 border-b border-slate-300">
+    <div className="rounded-xl border border-slate-200">
+      <h3 className="font-semibold text-slate-700 text-lg px-3 py-2 border-b border-slate-200">
         Price
       </h3>
 
       <div className="flex justify-between gap-3 text-sm px-3 pt-3">
-        <span className="flex-1 p-2 border border-slate-300">{values[0]}</span>
-        <span className="flex-1 p-2 border border-slate-300">{values[1]}</span>
+        <span className="flex-1 p-2 border border-slate-200">{values[0]}</span>
+        <span className="flex-1 p-2 border border-slate-200">{values[1]}</span>
       </div>
 
       <div className="pt-5 pb-5 px-3 w-full">
