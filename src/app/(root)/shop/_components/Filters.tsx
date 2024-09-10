9e2298204fbbe6ae0faf8337be6a6644e963c1cf
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-10 20:15:32 +0600

    Refactor Filters component to add range filters for price, camera, display size, and weight, and dropdown and checkbox filters for connectivity and compatibility

--- src/app/(root)/shop/_components/Filters.tsx
+++ src/app/(root)/shop/_components/Filters.tsx
@@ -1,9 +1,18 @@
-import PriceFilter from "./PriceFilter";
+import { compatibilities, connectivities } from "@/constants";
+import CheckBoxFilterCard from "./CheckBoxFilterCard";
+import DropDownFilterCard from "./DropDownFilterCard";
+import RangeFilter from "./RangeFilter";
 
 const Filters = () => {
   return (
     <div className="flex flex-col gap-3">
-      <PriceFilter />
+      <RangeFilter field="price" min={0} max={300000} step={500} />
+      <DropDownFilterCard />
+      <CheckBoxFilterCard items={connectivities} field="connectivity" />
+      <CheckBoxFilterCard items={compatibilities} field="compatibility" />
+      <RangeFilter field="camera" min={0} max={200} />
+      <RangeFilter field="displaySize" min={0} max={27} step={0.5} />
+      <RangeFilter field="weight" min={0} max={5} step={0.5} />
     </div>
   );
 };
