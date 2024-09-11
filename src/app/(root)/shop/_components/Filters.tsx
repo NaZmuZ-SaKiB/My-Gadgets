14a6abf5886d8aa277163f1f4b7b95492c029435
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-11 16:49:15 +0600

    Refactor filter component names to use proper capitalization and update range values

--- src/app/(root)/shop/_components/Filters.tsx
+++ src/app/(root)/shop/_components/Filters.tsx
@@ -6,13 +6,13 @@ import RangeFilter from "./RangeFilter";
 const Filters = () => {
   return (
     <div className="flex flex-col gap-3">
-      <RangeFilter field="price" min={0} max={300000} step={500} />
+      <RangeFilter field="Price" min={0} max={300000} step={500} />
       <DropDownFilterCard />
       <CheckBoxFilterCard items={connectivities} field="connectivity" />
       <CheckBoxFilterCard items={compatibilities} field="compatibility" />
-      <RangeFilter field="camera" min={0} max={200} />
-      <RangeFilter field="displaySize" min={0} max={27} step={0.5} />
-      <RangeFilter field="weight" min={0} max={5} step={0.5} />
+      <RangeFilter field="Camera" min={0} max={200} />
+      <RangeFilter field="DisplaySize" min={0} max={27} step={0.5} />
+      <RangeFilter field="Weight" min={0} max={10} step={0.5} />
     </div>
   );
 };
