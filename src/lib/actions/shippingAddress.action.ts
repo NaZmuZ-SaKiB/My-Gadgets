2d9dbd654c6195cec2927ee1f77d4f2e6fad8480
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-17 15:47:16 +0600

    feat: Add shipping address actions

---
+++ src/lib/actions/shippingAddress.action.ts
@@ -0,0 +1,96 @@
+import { z } from "zod";
+import { ShippingAddressValidation } from "../validations/shippingAddress.validation";
+import { authKey, backendUrl } from "@/constants";
+import { cookies } from "next/headers";
+
+export const shippingAddressCreateAction = async (
+  payload: z.infer<typeof ShippingAddressValidation.create>
+) => {
+  const response = await fetch(`${backendUrl}/api/shipping-address`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: cookies().get(authKey)?.value || "",
+    },
+    body: JSON.stringify(payload),
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  return result;
+};
+
+export const shippingAddressUpdateAction = async ({
+  id,
+  payload,
+}: {
+  id: string;
+  payload: Partial<z.infer<typeof ShippingAddressValidation.create>>;
+}) => {
+  const response = await fetch(`${backendUrl}/api/shipping-address/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: cookies().get(authKey)?.value || "",
+    },
+    body: JSON.stringify(payload),
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  return result;
+};
+
+export const shippingAddressGetAllAction = async (params: string) => {
+  const response = await fetch(`${backendUrl}/api/shipping-address?${params}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: cookies().get(authKey)?.value || "",
+    },
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  if (!result?.success) {
+    return {
+      data: [],
+      total: 0,
+    };
+  }
+
+  return result;
+};
+
+export const shippingAddressGetByIdAction = async (id: string) => {
+  const response = await fetch(`${backendUrl}/api/shipping-address/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: cookies().get(authKey)?.value || "",
+    },
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  return result;
+};
+
+export const shippingAddressDeleteAction = async (id: string) => {
+  const response = await fetch(`${backendUrl}/api/shipping-address/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: cookies().get(authKey)?.value || "",
+    },
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  return result;
+};
