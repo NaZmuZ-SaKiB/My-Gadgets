a7ca8d7129211952a49251a0fcc87c8554a45ea6
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-18 12:08:14 +0600

    feat: Add AddShippingAddressModal component

---
+++ src/app/(root)/checkout/_components/AddShippingAddressModal.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import MGButton from "@/components/global/shared/MGButton";
+import ShippingAddressForm from "@/components/global/shared/ShippingAddressForm";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Plus } from "lucide-react";
+import { useState } from "react";
+
+const AddShippingAddressModal = () => {
+  const [open, setOpen] = useState(false);
+
+  const closeModal = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <MGButton className="rounded-md gap-2">
+          <Plus className="size-5" />
+          <span>Add New</span>
+        </MGButton>
+      </DialogTrigger>
+
+      <DialogContent className="p-4 !rounded-2xl">
+        <DialogHeader>
+          <DialogTitle className="text-lg font-semibold text-slate-700">
+            New Shipping Address
+          </DialogTitle>
+        </DialogHeader>
+
+        <ShippingAddressForm closeModal={closeModal} />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddShippingAddressModal;
