ba4796855ac32a644f57d94ea81bef7ae5d39297
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-18 23:38:35 +0600

    refactor: Update PaymentMethodSection component to include transaction ID input for bank transfer payment method

--- src/app/(root)/checkout/_components/PaymentMethodSection.tsx
+++ src/app/(root)/checkout/_components/PaymentMethodSection.tsx
@@ -1,16 +1,19 @@
 "use client";
 
+import { Input } from "@/components/ui/input";
 import { paymentMethods } from "@/constants";
 import { TPaymentMethod } from "@/types/order.type";
 
 type TProps = {
   selectedPaymentMethod: TPaymentMethod;
   setSelectedPaymentMethod: (paymentMethod: TPaymentMethod) => void;
+  setTransactionId: (transactionId: string) => void;
 };
 
 const PaymentMethodSection = ({
   selectedPaymentMethod,
   setSelectedPaymentMethod,
+  setTransactionId,
 }: TProps) => {
   return (
     <div className="sm:rounded-xl sm:border sm:border-slate-200 sm:p-4">
@@ -38,6 +41,16 @@ const PaymentMethodSection = ({
             </label>
           </div>
         ))}
+
+        {selectedPaymentMethod === "bank-transfer" && (
+          <div>
+            <p className="text-sm font-medium mb-1">Transaction ID</p>
+            <Input
+              onChange={(e) => setTransactionId(e.target.value)}
+              className="no-focus"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
