923ff025939647d60d91a8fc625ba2575b0263f7
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-18 11:43:44 +0600

    refactor: Update validation messages in shippingAddress.validation.ts

--- src/lib/validations/shippingAddress.validation.ts
+++ src/lib/validations/shippingAddress.validation.ts
@@ -1,19 +1,13 @@
 import { z } from "zod";
 
 const create = z.object({
-  addressLine1: z.string({
-    required_error: "Address is required.",
-  }),
+  addressLine1: z.string().min(1, { message: "Address is required." }),
   addressLine2: z.string().optional(),
-  city: z.string({ required_error: "City is required." }),
-  district: z.string({ required_error: "District is required." }),
-  division: z.string({ required_error: "Division is required." }),
-  zipCode: z.string({
-    required_error: "Zip Code is required.",
-  }),
-  phone: z.string({
-    required_error: "Phone is required.",
-  }),
+  city: z.string().min(1, { message: "City is required." }),
+  district: z.string().min(1, { message: "District is required." }),
+  division: z.string().min(1, { message: "Division is required." }),
+  zipCode: z.string().min(1, { message: "Zip code is required." }),
+  phone: z.string().min(1, { message: "Phone number is required." }),
   default: z.boolean().optional(),
 });
 
