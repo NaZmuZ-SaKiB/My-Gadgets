1991023d00c32db0242df8c5b2468f3a5e813233
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-18 20:01:37 +0600

    refactor: Update auth.query.ts to add useQuery hook and AQTags constant

--- src/lib/queries/auth.query.ts
+++ src/lib/queries/auth.query.ts
@@ -1,7 +1,14 @@
-import { useMutation } from "@tanstack/react-query";
-import { signIn } from "../actions/auth.action";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { isUserLoggedIn, signIn } from "../actions/auth.action";
+import { AQTags } from "@/constants";
 
 export const useSignInMutation = () =>
   useMutation({
     mutationFn: signIn,
+  });
+
+export const useIsUserLoggedInQuery = () =>
+  useQuery({
+    queryKey: [],
+    queryFn: () => isUserLoggedIn(),
   });
