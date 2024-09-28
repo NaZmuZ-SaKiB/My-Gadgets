b1b2c3e2118050c8e37c50a79061d420c5623009
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-28 23:41:00 +0600

    refactor: Update AccountLayout component to include AccountSidebar

--- src/app/(root)/account/layout.tsx
+++ src/app/(root)/account/layout.tsx
@@ -1,9 +1,15 @@
 import BreadcrumbBar from "@/components/global/shared/Breadcrumb";
+import AccountSidebar from "./_components/AccountSidebar";
 
 const AccountLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="mg-container p-4">
       <BreadcrumbBar items={[{ label: "My Account" }]} />
+
+      <div className="grid lg:grid-cols-[220px_1fr] mt-5">
+        <AccountSidebar />
+        <div></div>
+      </div>
     </div>
   );
 };
