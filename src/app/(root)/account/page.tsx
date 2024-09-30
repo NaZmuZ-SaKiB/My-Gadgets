207f7fe1cb7222feee9ad9b06bdb358b11a88800
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-30 20:17:01 +0600

    refactor: Update AccountPage component to include useCurrentUserQuery hook

--- src/app/(root)/account/page.tsx
+++ src/app/(root)/account/page.tsx
@@ -1,4 +1,11 @@
+"use client";
+
+import { useCurrentUserQuery } from "@/lib/queries/auth.query";
+
 const AccountPage = () => {
+  const { data: user, isLoading } = useCurrentUserQuery();
+
+  console.log(user);
   return (
     <div>
       <h1>AccountPage</h1>
