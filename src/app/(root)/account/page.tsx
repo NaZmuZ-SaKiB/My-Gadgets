7d0b11e701f549267823df8d07154ca3a9d1d467
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-10-01 23:34:25 +0600

    refactor: Update AccountPage component to include form for updating user information

--- src/app/(root)/account/page.tsx
+++ src/app/(root)/account/page.tsx
@@ -1,14 +1,43 @@
 "use client";
 
+import MGForm from "@/components/global/forms/MGForm";
+import MGInput from "@/components/global/forms/MGInput";
+import MGButton from "@/components/global/shared/MGButton";
 import { useCurrentUserQuery } from "@/lib/queries/auth.query";
+import { SubmitHandler } from "react-hook-form";
 
 const AccountPage = () => {
   const { data: user, isLoading } = useCurrentUserQuery();
 
-  console.log(user);
+  const handleUpdate: SubmitHandler<{ name: string; email: string }> = async (
+    values
+  ) => {
+    console.log(values);
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const defaultValues = {
+    name: user?.name || "",
+    email: user?.email,
+  };
+
   return (
-    <div>
-      <h1>AccountPage</h1>
+    <div className="max-w-md mx-auto">
+      <MGForm
+        onSubmit={handleUpdate}
+        defaultValues={defaultValues}
+        reset={false}
+      >
+        <MGInput name="name" label="Name" />
+        <MGInput name="email" label="Email" />
+
+        <MGButton type="submit" className="self-start">
+          Save Changes
+        </MGButton>
+      </MGForm>
     </div>
   );
 };
