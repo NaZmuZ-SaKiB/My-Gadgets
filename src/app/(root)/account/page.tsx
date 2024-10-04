3decd185b8f7364cb0997a3ba122897c90438a22
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-10-04 17:44:58 +0600

    refactor: Update AccountPage component to include useMyProfileUpdateMutation hook and handle form submission

--- src/app/(root)/account/page.tsx
+++ src/app/(root)/account/page.tsx
@@ -4,15 +4,35 @@ import MGForm from "@/components/global/forms/MGForm";
 import MGInput from "@/components/global/forms/MGInput";
 import MGButton from "@/components/global/shared/MGButton";
 import { useCurrentUserQuery } from "@/lib/queries/auth.query";
+import { useMyProfileUpdateMutation } from "@/lib/queries/user.query";
 import { SubmitHandler } from "react-hook-form";
+import { toast } from "sonner";
 
 const AccountPage = () => {
-  const { data: user, isLoading } = useCurrentUserQuery();
+  const { data: user, isLoading, refetch } = useCurrentUserQuery();
+
+  const { mutateAsync: updateAccount, isPending } =
+    useMyProfileUpdateMutation();
 
   const handleUpdate: SubmitHandler<{ name: string; email: string }> = async (
     values
   ) => {
-    console.log(values);
+    if (user?.name === values.name && user?.email === values.email) {
+      return;
+    }
+
+    try {
+      const result = await updateAccount(values);
+
+      if (result?.success) {
+        toast.success(result?.message);
+        refetch();
+      } else {
+        toast.error(result?.message || "A server error occured");
+      }
+    } catch (error: any) {
+      toast.error(error?.message || "A client error occured");
+    }
   };
 
   if (isLoading) {
@@ -34,8 +54,8 @@ const AccountPage = () => {
         <MGInput name="name" label="Name" />
         <MGInput name="email" label="Email" />
 
-        <MGButton type="submit" className="self-start">
-          Save Changes
+        <MGButton type="submit" className="self-start" disabled={isPending}>
+          {isPending ? "Saving..." : "Save Changes"}
         </MGButton>
       </MGForm>
     </div>
