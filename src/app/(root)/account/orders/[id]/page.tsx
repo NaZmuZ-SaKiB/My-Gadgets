117f1f5cdd8f2a4a12cf5b4c9d12f82bc4439f0d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-10-09 19:53:05 +0600

    feat: Add SingleOrderPage component

---
+++ src/app/(root)/account/orders/[id]/page.tsx
@@ -0,0 +1,15 @@
+type TProps = {
+  params: {
+    id: string;
+  };
+};
+
+const SingleOrderPage = ({ params }: TProps) => {
+  return (
+    <div>
+      <h1>SingleOrderPage</h1>
+    </div>
+  );
+};
+
+export default SingleOrderPage;
