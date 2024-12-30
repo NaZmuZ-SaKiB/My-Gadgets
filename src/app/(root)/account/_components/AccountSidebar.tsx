68ee404368a4fcfdecc6b397cc7a163dea25112e
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-12-30 22:54:52 +0600

    refactor: Enhance responsiveness of AccountSidebar component layout

--- src/app/(root)/account/_components/AccountSidebar.tsx
+++ src/app/(root)/account/_components/AccountSidebar.tsx
@@ -9,20 +9,20 @@ const AccountSidebar = () => {
   const pathName = usePathname();
 
   return (
-    <div className="flex flex-col gap-3 justify-center">
+    <div className="flex lg:flex-col gap-3 justify-center max-sm:flex-wrap">
       {accountSideBar.map((item) => (
         <Link
           key={item.link}
           href={item.link}
           className={cn(
-            "px-2 py-1 flex items-center border border-slate-200 rounded-lg cursor-pointer",
+            "px-1 sm:px-2 py-1 flex max-sm:flex-1 max-xs:basis-[150px] max-sm:basis-[200px] items-center border border-slate-200 rounded-lg cursor-pointer",
             {
               group: pathName !== item.link,
               "bg-primary text-white [&>*]:text-white": pathName === item.link,
             }
           )}
         >
-          <span className="p-2 text-slate-500 group-hover:text-primary-hover">
+          <span className="max-sm:hidden p-2 text-slate-500 group-hover:text-primary-hover">
             <item.icon />
           </span>
           <span className="p-2 text-sm text-slate-700 font-semibold group-hover:text-primary-hover">
