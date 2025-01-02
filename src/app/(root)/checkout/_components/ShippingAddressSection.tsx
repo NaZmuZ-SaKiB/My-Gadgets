5ccb642442c9dcba12170b3660c45f1d04bb3bba
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-02 18:25:22 +0600

    feat: Refactor ShippingAddressSection component for improved code organization and styling consistency

--- src/app/(root)/checkout/_components/ShippingAddressSection.tsx
+++ src/app/(root)/checkout/_components/ShippingAddressSection.tsx
@@ -2,7 +2,7 @@
 
 import { useShippingAddressGetAllQuery } from "@/lib/queries/shippingAddress.query";
 import { TShippingAddress } from "@/types/shippingAddress.type";
-import AddShippingAddressModal from "./AddShippingAddressModal";
+import AddShippingAddressModal from "@/components/global/shared/AddShippingAddressModal";
 import { RefreshCcw } from "lucide-react";
 import { useEffect } from "react";
 import { cn } from "@/lib/utils";
@@ -19,14 +19,14 @@ const ShippingAddressSection = ({
   setSelectedAddress,
 }: TProps) => {
   const { data, isLoading, refetch } = useShippingAddressGetAllQuery(
-    `user=${userId}`
+    `user=${userId}`,
   );
   const addresses: TShippingAddress[] = data?.data || [];
 
   useEffect(() => {
     if (data?.data.length > 0) {
       const defaultAddress = data?.data.find(
-        (address: TShippingAddress) => address.default
+        (address: TShippingAddress) => address.default,
       );
       setSelectedAddress(defaultAddress!._id?.toString());
     }
@@ -34,13 +34,13 @@ const ShippingAddressSection = ({
 
   if (isLoading) return <div className="row-span-2">Loading...</div>;
   return (
-    <div className="sm:rounded-xl sm:border sm:border-slate-200 sm:p-4 lg:row-span-2 max-lg:col-span-2 max-lg:border-b max-lg:pb-4">
-      <div className="flex items-center justify-between gap-3 mb-3">
+    <div className="max-lg:col-span-2 max-lg:border-b max-lg:pb-4 sm:rounded-xl sm:border sm:border-slate-200 sm:p-4 lg:row-span-2">
+      <div className="mb-3 flex items-center justify-between gap-3">
         <h2 className="text-lg font-semibold text-slate-700">
           Shipping Address
         </h2>
         <span
-          className="flex items-center gap-2 text-slate-700 text-sm px-2 py-0.5 border border-slate-200 rounded cursor-pointer"
+          className="flex cursor-pointer items-center gap-2 rounded border border-slate-200 px-2 py-0.5 text-sm text-slate-700"
           onClick={() => refetch()}
         >
           <RefreshCcw className="size-4" /> <span>Refresh</span>
@@ -52,11 +52,11 @@ const ShippingAddressSection = ({
           <div
             key={`${address?._id}`}
             className={cn(
-              "border-2 border-slate-50 bg-slate-50 text-sm text-slate-700 p-2 cursor-pointer rounded-lg",
+              "cursor-pointer rounded-lg border-2 border-slate-50 bg-slate-50 p-2 text-sm text-slate-700",
               {
                 "border-primary/50":
                   selectedAddress === address?._id?.toString(),
-              }
+              },
             )}
             onClick={() => setSelectedAddress(address?._id?.toString())}
           >
