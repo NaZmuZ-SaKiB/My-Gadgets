f37be0304eb4c09f2839adb5975548c210500998
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-02 18:25:05 +0600

    feat: Create MyAddressesPage component to display and manage user shipping addresses

---
+++ src/app/(root)/account/addresses/page.tsx
@@ -0,0 +1,50 @@
+import AddShippingAddressModal from "@/components/global/shared/AddShippingAddressModal";
+import { currentUser, isUserLoggedIn } from "@/lib/actions/auth.action";
+import { shippingAddressGetAllAction } from "@/lib/actions/shippingAddress.action";
+import { TShippingAddress } from "@/types/shippingAddress.type";
+
+const MyAddressesPage = async () => {
+  const user = await isUserLoggedIn();
+
+  const addressData = await shippingAddressGetAllAction(`user=${user?._id}`);
+  const addresses: TShippingAddress[] = addressData?.data || [];
+
+  return (
+    <div className="lg:mg-container">
+      <h1 className="mb-4 text-3xl font-semibold text-slate-700">
+        Your Addresses
+      </h1>
+
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        {addresses.length > 0
+          ? addresses.map((address) => (
+              <div
+                key={`${address?._id}`}
+                className={
+                  "cursor-pointer rounded-lg border-2 border-slate-50 bg-slate-50 p-2 text-sm text-slate-700"
+                }
+              >
+                <p>{address?.addressLine1}</p>
+                {address?.addressLine2 && <p>{address?.addressLine2}</p>}
+                <p>
+                  <span>City: {address.city}</span>
+                  {", "}
+                  <span>District: {address.district}</span>
+                </p>
+                <p>
+                  <span>Division: {address.division}</span>
+                  {", "}
+                  <span>Zip: {address.zipCode}</span>
+                </p>
+                <p>Phone: {address.phone}</p>
+              </div>
+            ))
+          : null}
+
+        {addresses.length <= 4 ? <AddShippingAddressModal /> : null}
+      </div>
+    </div>
+  );
+};
+
+export default MyAddressesPage;
