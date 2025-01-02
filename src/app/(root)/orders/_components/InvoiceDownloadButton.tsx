a40024093247f68642cd755e2dad2fcd0a007d37
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-02 17:28:31 +0600

    feat: Enhance InvoiceDownloadButton to generate PDF invoices using html2canvas and jsPDF

--- src/app/(root)/orders/_components/InvoiceDownloadButton.tsx
+++ src/app/(root)/orders/_components/InvoiceDownloadButton.tsx
@@ -1,17 +1,47 @@
 "use client";
 
+import html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
 import MGButton from "@/components/global/shared/MGButton";
 
 const InvoiceDownloadButton = () => {
-  const handleClick = () => {
+  const handleClick = async () => {
     const invoice = document.getElementById("invoice");
 
-    if (invoice) {
-    }
+    if (!invoice) return;
+
+    const style = document.createElement("style");
+    document.head.appendChild(style);
+    style.sheet?.insertRule(
+      "body > div:last-child img { display: inline-block; }",
+    );
+
+    const canvas = await html2canvas(invoice, {
+      scale: 2,
+    });
+
+    style.remove();
+
+    const data = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF({
+      orientation: "portrait",
+      unit: "px",
+      format: "a4",
+    });
+
+    const imgProperties = pdf.getImageProperties(data);
+
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+
+    const pdfHeight = (imgProperties.height * pdfWidth) / imgProperties.width;
+
+    pdf.addImage(data, "PNG", 0, 0, pdfWidth, pdfHeight);
+    pdf.save("invoice.pdf");
   };
 
   return (
-    <div className="mx-auto flex max-w-screen-sm justify-center">
+    <div className="mx-auto mt-4 flex max-w-screen-sm justify-center">
       <MGButton className="rounded-md" onClick={handleClick}>
         Download
       </MGButton>
