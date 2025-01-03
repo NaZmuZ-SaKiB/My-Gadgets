5bf427c375511e5c806a00eaf32ddec0e4a7f107
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-03 20:19:32 +0600

    feat: Integrate EditShippingAddressModal into MyAddressesPage for address editing functionality

--- src/app/(root)/account/addresses/page.tsx
+++ src/app/(root)/account/addresses/page.tsx
@@ -2,6 +2,7 @@ import AddShippingAddressModal from "@/components/global/shared/AddShippingAddre
 import { currentUser, isUserLoggedIn } from "@/lib/actions/auth.action";
 import { shippingAddressGetAllAction } from "@/lib/actions/shippingAddress.action";
 import { TShippingAddress } from "@/types/shippingAddress.type";
+import EditShippingAddressModal from "./_components/EditShippingAddressModal";
 
 const MyAddressesPage = async () => {
   const user = await isUserLoggedIn();
@@ -21,22 +22,28 @@ const MyAddressesPage = async () => {
               <div
                 key={`${address?._id}`}
                 className={
-                  "cursor-pointer rounded-lg border-2 border-slate-50 bg-slate-50 p-2 text-sm text-slate-700"
+                  "flex cursor-pointer flex-col justify-between rounded-lg border-2 border-slate-50 bg-slate-50 p-2 text-sm text-slate-700"
                 }
               >
-                <p>{address?.addressLine1}</p>
-                {address?.addressLine2 && <p>{address?.addressLine2}</p>}
-                <p>
-                  <span>City: {address.city}</span>
-                  {", "}
-                  <span>District: {address.district}</span>
-                </p>
-                <p>
-                  <span>Division: {address.division}</span>
-                  {", "}
-                  <span>Zip: {address.zipCode}</span>
-                </p>
-                <p>Phone: {address.phone}</p>
+                <div>
+                  <p>{address?.addressLine1}</p>
+                  {address?.addressLine2 && <p>{address?.addressLine2}</p>}
+                  <p>
+                    <span>City: {address.city}</span>
+                    {", "}
+                    <span>District: {address.district}</span>
+                  </p>
+                  <p>
+                    <span>Division: {address.division}</span>
+                    {", "}
+                    <span>Zip: {address.zipCode}</span>
+                  </p>
+                  <p>Phone: {address.phone}</p>
+                </div>
+
+                <div className="mt-4">
+                  <EditShippingAddressModal currentAddress={address} />
+                </div>
               </div>
             ))
           : null}
