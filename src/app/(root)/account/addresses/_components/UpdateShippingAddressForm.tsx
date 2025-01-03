6bf3c42d230b3d1ffc18c3ed929d36c3b88d2268
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-03 20:19:45 +0600

    feat: Add UpdateShippingAddressForm component for updating shipping addresses

---
+++ src/app/(root)/account/addresses/_components/UpdateShippingAddressForm.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { ShippingAddressValidation } from "@/lib/validations/shippingAddress.validation";
+import { SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import MGForm from "@/components/global/forms/MGForm";
+import { zodResolver } from "@hookform/resolvers/zod";
+import MGInput from "@/components/global/forms/MGInput";
+import MGButton from "@/components/global/shared/MGButton";
+import {
+  useShippingAddressCreateMutation,
+  useShippingAddressUpdateMutation,
+} from "@/lib/queries/shippingAddress.query";
+import { toast } from "sonner";
+import { useQueryClient } from "@tanstack/react-query";
+import { AQTags } from "@/constants";
+import { TShippingAddress } from "@/types/shippingAddress.type";
+
+type TProps = {
+  currentAddress: TShippingAddress;
+  closeModal?: () => void;
+};
+
+const UpdateShippingAddressForm = ({ closeModal, currentAddress }: TProps) => {
+  const queryClient = useQueryClient();
+
+  const { mutateAsync: updateShippingAddress, isPending } =
+    useShippingAddressUpdateMutation();
+
+  const handleSubmit: SubmitHandler<
+    z.infer<typeof ShippingAddressValidation.create>
+  > = async (values) => {
+    try {
+      const result = await updateShippingAddress({
+        id: currentAddress._id,
+        payload: values,
+      });
+
+      if (result?.success) {
+        queryClient.invalidateQueries({
+          queryKey: [AQTags.SHIPPING_ADDRESS, AQTags.ALL],
+          exact: false,
+        });
+
+        toast.success(result.message);
+      } else {
+        toast.error(result?.message || "A Server Error Occurred.");
+      }
+
+      if (closeModal) {
+        closeModal();
+      }
+    } catch (error: any) {
+      toast.error(error?.message || "A Client Error Occurred.");
+    }
+  };
+
+  const defaultValues = {
+    addressLine1: currentAddress.addressLine1 || "",
+    addressLine2: currentAddress.addressLine2 || "",
+    city: currentAddress.city || "",
+    district: currentAddress.district || "",
+    division: currentAddress.division || "",
+    zipCode: currentAddress.zipCode || "",
+    phone: currentAddress.phone || "",
+  };
+
+  return (
+    <MGForm
+      onSubmit={handleSubmit}
+      resolver={zodResolver(ShippingAddressValidation.create)}
+      defaultValues={defaultValues}
+      reset={false}
+    >
+      <MGInput name="addressLine1" label="Address Line 1" showLabel={false} />
+      <MGInput name="addressLine2" label="Address Line 2" showLabel={false} />
+      <MGInput name="city" label="City" showLabel={false} />
+      <MGInput name="district" label="District" showLabel={false} />
+      <MGInput name="division" label="Division" showLabel={false} />
+      <MGInput name="zipCode" label="Zip Code" showLabel={false} />
+      <MGInput name="phone" label="Phone" showLabel={false} />
+
+      <MGButton className="h-auto py-3" disabled={isPending}>
+        {isPending ? "Saving..." : "Save"}
+      </MGButton>
+    </MGForm>
+  );
+};
+
+export default UpdateShippingAddressForm;
