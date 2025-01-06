aa9bd8a167f27172a38f4ea20890e3101066fbe1
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-06 22:20:01 +0600

    refactor: Simplify import statement and improve layout in AccountSidebar component

--- src/app/(root)/account/_components/AccountSidebar.tsx
+++ src/app/(root)/account/_components/AccountSidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { accountSideBar } from "@/constants/accountSidebar";
+import { accountSideBar } from "@/constants";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -9,23 +9,23 @@ const AccountSidebar = () => {
   const pathName = usePathname();
 
   return (
-    <div className="flex lg:flex-col gap-3 justify-center max-sm:flex-wrap">
+    <div className="flex justify-center gap-3 max-sm:flex-wrap lg:flex-col">
       {accountSideBar.map((item) => (
         <Link
           key={item.link}
           href={item.link}
           className={cn(
-            "px-1 sm:px-2 py-1 flex max-sm:flex-1 max-xs:basis-[150px] max-sm:basis-[200px] items-center border border-slate-200 rounded-lg cursor-pointer",
+            "flex cursor-pointer items-center rounded-lg border border-slate-200 px-1 py-1 max-sm:flex-1 max-sm:basis-[200px] max-xs:basis-[150px] sm:px-2",
             {
               group: pathName !== item.link,
               "bg-primary text-white [&>*]:text-white": pathName === item.link,
-            }
+            },
           )}
         >
-          <span className="max-sm:hidden p-2 text-slate-500 group-hover:text-primary-hover">
+          <span className="p-2 text-slate-500 group-hover:text-primary-hover max-sm:hidden">
             <item.icon />
           </span>
-          <span className="p-2 text-sm text-slate-700 font-semibold group-hover:text-primary-hover">
+          <span className="p-2 text-sm font-semibold text-slate-700 group-hover:text-primary-hover">
             {item.name}
           </span>
         </Link>
