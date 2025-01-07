f02d79a7839fe612f3824406d17c377ea5f7f0fe
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-07 22:09:32 +0600

    feat: Enhance Account component with user authentication state and logout functionality

--- src/components/global/layout/header/Account.tsx
+++ src/components/global/layout/header/Account.tsx
@@ -8,47 +8,86 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { icons } from "@/constants";
-import { LogOut, ShoppingBag, User } from "lucide-react";
+import {
+  useCurrentUserQuery,
+  useSignOutMutation,
+} from "@/lib/queries/auth.query";
+import { LogIn, LogOut, ShoppingBag, User, UserCircle } from "lucide-react";
 
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const Account = () => {
   const [open, setOpen] = useState(false);
 
+  const { data: user } = useCurrentUserQuery();
+
+  const { mutateAsync: logoutFn, isPending } = useSignOutMutation();
+
+  const router = useRouter();
+
+  const logout = async () => {
+    await logoutFn();
+
+    router.push("/");
+  };
+
   return (
     <DropdownMenu open={open} onOpenChange={setOpen}>
       <DropdownMenuTrigger className="no-focus cursor-pointer">
-        <Image src={icons.user} alt="account" className="size-6 mx-auto" />
+        <Image src={icons.user} alt="account" className="mx-auto size-6" />
         <span className="text-sm">Account</span>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent>
-        <DropdownMenuLabel>Nazmuz Sakib</DropdownMenuLabel>
-
-        <DropdownMenuItem className="gap-2">
-          <span>
-            <User className="size-4" />
-          </span>
-          <Link href="/account" className="w-full">
-            Profile
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem className="gap-2">
-          <span>
-            <ShoppingBag className="size-4" />
-          </span>
-          <Link href="/account/orders" className="w-full">
-            My Orders
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem className="gap-2 cursor-pointer">
-          <span>
-            <LogOut className="size-4" />
-          </span>
-          <span>Logout</span>
-        </DropdownMenuItem>
+        {user ? (
+          <>
+            <DropdownMenuLabel>Nazmuz Sakib</DropdownMenuLabel>
+            <DropdownMenuItem className="gap-2">
+              <span>
+                <User className="size-4" />
+              </span>
+              <Link href="/account" className="w-full">
+                Profile
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="gap-2">
+              <span>
+                <ShoppingBag className="size-4" />
+              </span>
+              <Link href="/account/orders" className="w-full">
+                My Orders
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer gap-2" onClick={logout}>
+              <span>
+                <LogOut className="size-4" />
+              </span>
+              <span>Logout</span>
+            </DropdownMenuItem>
+          </>
+        ) : (
+          <>
+            <DropdownMenuItem className="gap-2">
+              <span>
+                <LogIn className="size-4" />
+              </span>
+              <Link href="/sign-in" className="w-full">
+                Log in
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="gap-2">
+              <span>
+                <UserCircle className="size-4" />
+              </span>
+              <Link href="/sign-up" className="w-full">
+                Create Account
+              </Link>
+            </DropdownMenuItem>
+          </>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
