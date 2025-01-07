79a50855b17cc8d8e325a662d8d63e26b530ce21
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-07 19:18:17 +0600

    feat: Add SignUpPage component to display the sign-up form and branding

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,32 @@
+import { images } from "@/constants";
+import Image from "next/image";
+import SignUpForm from "./SignUpForm";
+
+const SignUpPage = () => {
+  return (
+    <main className="grid min-h-svh place-items-center bg-white xs:bg-slate-50">
+      <div className="mx-auto flex w-full max-w-[400px] flex-col gap-4 bg-white px-4 py-10 shadow-slate-300 xs:rounded-2xl xs:shadow-2xl">
+        <div className="flex items-center justify-center gap-3">
+          <div className="rounded-full border border-primary p-2">
+            <Image src={images.logo} width={40} height={40} alt="My Gadgets" />
+          </div>
+
+          <h1 className="text-lg font-semibold">My Gadgets</h1>
+        </div>
+
+        <div className="mb-5">
+          <h2 className="mb-1 text-center text-3xl font-semibold text-slate-800">
+            Welcome!
+          </h2>
+          <p className="text-center text-xs text-slate-500">
+            Create your account
+          </p>
+        </div>
+
+        <SignUpForm />
+      </div>
+    </main>
+  );
+};
+
+export default SignUpPage;
