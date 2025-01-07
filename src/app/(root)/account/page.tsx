da2a5d3a5140585c08809c3065382781d0752643
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-07 20:31:17 +0600

    feat: Refactor AccountPage to use AccountForm component and async user fetching

--- src/app/(root)/account/page.tsx
+++ src/app/(root)/account/page.tsx
@@ -1,47 +1,12 @@
-"use client";
+import { currentUser } from "@/lib/actions/auth.action";
+import AccountForm from "./_components/AccountForm";
 
-import MGForm from "@/components/global/forms/MGForm";
-import MGInput from "@/components/global/forms/MGInput";
-import MGButton from "@/components/global/shared/MGButton";
-import { useCurrentUserQuery } from "@/lib/queries/auth.query";
-import { useMyProfileUpdateMutation } from "@/lib/queries/user.query";
-import { SubmitHandler } from "react-hook-form";
-import { toast } from "sonner";
-
-const AccountPage = () => {
-  const { data: user, isLoading, refetch } = useCurrentUserQuery();
-
-  const { mutateAsync: updateAccount, isPending } =
-    useMyProfileUpdateMutation();
-
-  const handleUpdate: SubmitHandler<{ name: string; email: string }> = async (
-    values,
-  ) => {
-    if (user?.name === values.name && user?.email === values.email) {
-      return;
-    }
-
-    try {
-      const result = await updateAccount(values);
-
-      if (result?.success) {
-        toast.success(result?.message);
-        refetch();
-      } else {
-        toast.error(result?.message || "A server error occured");
-      }
-    } catch (error: any) {
-      toast.error(error?.message || "A client error occured");
-    }
-  };
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+const AccountPage = async () => {
+  const user = await currentUser();
 
   const defaultValues = {
     name: user?.name || "",
-    email: user?.email,
+    email: user?.email || "",
   };
 
   return (
@@ -50,18 +15,7 @@ const AccountPage = () => {
         Your Account
       </h1>
 
-      <MGForm
-        onSubmit={handleUpdate}
-        defaultValues={defaultValues}
-        reset={false}
-      >
-        <MGInput name="name" label="Name" />
-        <MGInput name="email" label="Email" />
-
-        <MGButton type="submit" className="self-start" disabled={isPending}>
-          {isPending ? "Saving..." : "Save Changes"}
-        </MGButton>
-      </MGForm>
+      <AccountForm defaultValues={defaultValues} />
     </div>
   );
 };
