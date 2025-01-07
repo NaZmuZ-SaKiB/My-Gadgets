5f7fa088754ebdf13ec2970ac9719dac83f1102e
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-07 16:57:17 +0600

    style: Refactor Footer component for improved layout and readability

--- src/components/global/layout/Footer/Footer.tsx
+++ src/components/global/layout/Footer/Footer.tsx
@@ -5,13 +5,13 @@ import { Clock, Mail } from "lucide-react";
 import Image from "next/image";
 
 const Footer = async () => {
-  const settings = await settingsGetAction();
+  const settings = await settingsGetAction("footer");
   const footerData: TFooterSettings = settings?.data?.footer;
 
   return (
-    <div className="border-t border-slate-300 pt-8 mt-4">
+    <div className="mt-4 border-t border-slate-300 pt-8">
       <footer className="container">
-        <div className="flex flex-col md:flex-row gap-x-4 gap-y-6">
+        <div className="flex flex-col gap-x-4 gap-y-6 md:flex-row">
           {/* Column 1 */}
           <div className="w-full">
             <div className="mb-5">
@@ -20,18 +20,18 @@ const Footer = async () => {
                 alt="logo"
                 width={100}
                 height={100}
-                className="max-md:mx-auto max-w-[200px]"
+                className="max-w-[200px] max-md:mx-auto"
               />
             </div>
-            <p className="text-slate-500 text-lg font-semibold mb-5 max-md:text-center lg:max-w-[90%] xl:max-w-[70%]">
+            <p className="mb-5 text-lg font-semibold text-slate-500 max-md:text-center lg:max-w-[90%] xl:max-w-[70%]">
               {footerData?.slogan}
             </p>
-            <p className="flex flex-wrap items-center gap-2 text-slate-500 max-md:justify-center text-sm">
+            <p className="flex flex-wrap items-center gap-2 text-sm text-slate-500 max-md:justify-center">
               <Mail className="size-5 text-primary" />
               <span className="font-semibold">Email:</span>
               <span>{footerData?.email}</span>
             </p>
-            <p className="flex items-center gap-2 text-slate-500 mt-3 max-md:justify-center text-sm">
+            <p className="mt-3 flex items-center gap-2 text-sm text-slate-500 max-md:justify-center">
               <Clock className="size-5 text-primary" />
               <span className="font-semibold">Hours:</span>
               <span>{footerData?.hours}</span>
@@ -39,9 +39,9 @@ const Footer = async () => {
           </div>
 
           {/* Column 2 */}
-          <div className="md:ml-10 w-full max-md:text-center">
+          <div className="w-full max-md:text-center md:ml-10">
             <h3 className="text-xl font-bold text-slate-700">Quick Links</h3>
-            <ul className="text-slate-500 mt-3 md:mt-6 space-y-2">
+            <ul className="mt-3 space-y-2 text-slate-500 md:mt-6">
               <li>Shop</li>
               <li>Contact</li>
               <li>About Us</li>
@@ -56,14 +56,14 @@ const Footer = async () => {
             <h3 className="text-xl font-bold text-slate-700">For Query</h3>
             <div
               dangerouslySetInnerHTML={{ __html: footerData?.contact }}
-              className="mt-3 flex flex-col text-sm gap-2 text-slate-700 leading-6"
+              className="mt-3 flex flex-col gap-2 text-sm leading-6 text-slate-700"
             />
           </div>
         </div>
       </footer>
       <div>
-        <hr className="border-slate-300 mt-8" />
-        <p className="text-slate-500 px-2 py-4 text-center text-sm">
+        <hr className="mt-8 border-slate-300" />
+        <p className="px-2 py-4 text-center text-sm text-slate-500">
           © 2024 My Gadgets. All Rights Reserved.
         </p>
       </div>
