155134345f842464d3dd17b53489f4d96212406c
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-10 18:44:27 +0600

    refactor: remove selection state from UserOrdersPage and simplify OrdersTable usage

--- src/app/admin/users/[id]/orders/page.tsx
+++ src/app/admin/users/[id]/orders/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import AFloatingBox from "@/components/admin/admin-ui/AFloatingBox";
 import APageContainer from "@/components/admin/admin-ui/APageContainer";
 import APageHeading from "@/components/admin/admin-ui/APageHeading";
 import OrdersTable from "@/components/admin/shared/OrdersTable";
@@ -8,13 +7,9 @@ import { useOrderGetAllQuery } from "@/lib/queries/order.query";
 import { useUserGetByIdQuery } from "@/lib/queries/user.query";
 import { TOrder } from "@/types/order.type";
 import { TUser } from "@/types/user.type";
-import { Loader2 } from "lucide-react";
 import { useParams } from "next/navigation";
-import { useState } from "react";
 
 const UserOrdersPage = () => {
-  const [selectedOrders, setSelectedOrders] = useState<string[]>([]);
-
   const params = useParams();
 
   const { data: userData, isLoading: userLoading } = useUserGetByIdQuery(
@@ -31,12 +26,7 @@ const UserOrdersPage = () => {
     <APageContainer>
       <APageHeading title={userLoading ? "Orders" : `${user.name}'s Orders`} />
 
-      <OrdersTable
-        selectedOrders={selectedOrders}
-        setSelectedOrders={setSelectedOrders}
-        orders={orders}
-        isLoading={orderLoading}
-      />
+      <OrdersTable orders={orders} isLoading={orderLoading} />
     </APageContainer>
   );
 };
