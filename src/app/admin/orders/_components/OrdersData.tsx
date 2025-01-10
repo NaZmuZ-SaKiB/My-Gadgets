9193b9f23aaee3ead0fd4600be2098bd42151127
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-10 13:11:18 +0600

    feat: Integrate order fetching in OrdersData component with loading state

--- src/app/admin/orders/_components/OrdersData.tsx
+++ src/app/admin/orders/_components/OrdersData.tsx
@@ -7,15 +7,22 @@ import DataSortOrderSelect from "@/components/admin/shared/filters/DataSortOrder
 import SelectedItemsCount from "@/components/admin/shared/SelectedItemsCount";
 import { orderSortOptions } from "@/constants";
 import { useState } from "react";
-import OrdersTable from "./OrdersTable";
+import OrdersTable from "@/components/admin/shared/OrdersTable";
+import { useSearchParams } from "next/navigation";
+import { useOrderGetAllQuery } from "@/lib/queries/order.query";
 
 const OrdersData = () => {
   const [selectedOrders, setSelectedOrders] = useState<string[]>([]);
 
+  const searchParams = useSearchParams();
+  const { data, isLoading } = useOrderGetAllQuery(searchParams.toString());
+
+  const orders = data?.data || [];
+
   return (
     <div className="flex flex-col gap-3">
       <AFloatingBox className="flex flex-col gap-2">
-        <div className="flex gap-2 flex-wrap">
+        <div className="flex flex-wrap gap-2">
           <SelectedItemsCount count={selectedOrders.length} />
 
           {/* Order Bulk Actions  */}
@@ -35,6 +42,8 @@ const OrdersData = () => {
       <OrdersTable
         selectedOrders={selectedOrders}
         setSelectedOrders={setSelectedOrders}
+        orders={orders}
+        isLoading={isLoading}
       />
     </div>
   );
