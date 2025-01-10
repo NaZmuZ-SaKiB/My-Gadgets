f7ddaa91292d29bd570d401c7c8aff82da21d00b
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-10 19:17:46 +0600

    feat: add OrderStatusFilter and OrderIsPaidFilter components to OrdersData

--- src/app/admin/orders/_components/OrdersData.tsx
+++ src/app/admin/orders/_components/OrdersData.tsx
@@ -4,12 +4,12 @@ import AFloatingBox from "@/components/admin/admin-ui/AFloatingBox";
 import DataLimitSelect from "@/components/admin/shared/filters/DataLimitSelect";
 import DataSortBySelect from "@/components/admin/shared/filters/DataSortBySelect";
 import DataSortOrderSelect from "@/components/admin/shared/filters/DataSortOrderSelect";
-import SelectedItemsCount from "@/components/admin/shared/SelectedItemsCount";
 import { orderSortOptions } from "@/constants";
-import { useState } from "react";
 import OrdersTable from "@/components/admin/shared/OrdersTable";
 import { useSearchParams } from "next/navigation";
 import { useOrderGetAllQuery } from "@/lib/queries/order.query";
+import OrderStatusFilter from "@/components/admin/shared/filters/OrderStatusFilter";
+import OrderIsPaidFilter from "@/components/admin/shared/filters/OrderIsPaidFilter";
 
 const OrdersData = () => {
   const searchParams = useSearchParams();
@@ -21,6 +21,9 @@ const OrdersData = () => {
     <div className="flex flex-col gap-3">
       <AFloatingBox className="flex flex-col gap-2">
         <div className="flex flex-wrap gap-2">
+          <OrderStatusFilter />
+          <OrderIsPaidFilter />
+
           <div className="sm:hidden">
             <DataLimitSelect />
           </div>
@@ -30,7 +33,7 @@ const OrdersData = () => {
           <DataSortOrderSelect />
         </div>
 
-        {/* Order Search Box */}
+        {/* //todo Order Search Box */}
       </AFloatingBox>
 
       <OrdersTable orders={orders} isLoading={isLoading} />
