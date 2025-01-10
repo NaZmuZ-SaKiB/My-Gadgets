989b9aed41dbff6e68b661e622e5da05f95f8e85
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-10 19:17:30 +0600

    feat: add OrderIsPaidFilter component for filtering orders by payment status

---
+++ src/components/admin/shared/filters/OrderIsPaidFilter.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+const OrderIsPaidFilter = () => {
+  const router = useRouter();
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+
+  const params = new URLSearchParams(searchParams);
+
+  const handleChange = (value: "all" | "true" | "false") => {
+    params.delete("isPaid");
+    params.delete("page");
+    params.append("page", "1");
+    if (value !== "all") {
+      params.append("isPaid", value);
+    }
+
+    router.replace(`${pathName}?${params}`);
+  };
+  return (
+    <Select
+      defaultValue={searchParams?.get("isPaid") || "all"}
+      onValueChange={handleChange}
+    >
+      <SelectTrigger className="no-focus w-[130px] gap-2 border-slate-200">
+        Paid:
+        <SelectValue />
+      </SelectTrigger>
+
+      <SelectContent>
+        <SelectItem value="all">All</SelectItem>
+        {["true", "false"].map((item) => (
+          <SelectItem
+            key={`order-status-select-${item}`}
+            value={item}
+            className="capitalize"
+          >
+            {item === "true" ? "Paid" : "Not Paid"}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default OrderIsPaidFilter;
