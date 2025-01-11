ee2c6f9fb088fd7977e9aa27fdf10db4fa09381c
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-11 21:17:54 +0600

    feat: implement logout functionality in ATopbar with AQTags for user state management

--- src/components/admin/layout/ATopbar.tsx
+++ src/components/admin/layout/ATopbar.tsx
@@ -1,12 +1,35 @@
-import { images } from "@/constants";
+"use client";
+
+import { AQTags, images } from "@/constants";
 import Image from "next/image";
 import ASidebarDrawer from "./ASidebarDrawer";
 import Link from "next/link";
+import { LogOut, User } from "lucide-react";
+import { useSignOutMutation } from "@/lib/queries/auth.query";
+import { useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 
 const ATopbar = () => {
+  const { mutateAsync: logoutFn } = useSignOutMutation();
+
+  const queryClient = useQueryClient();
+
+  const router = useRouter();
+
+  const logout = async () => {
+    await logoutFn();
+
+    queryClient.invalidateQueries({
+      queryKey: [AQTags.USER, AQTags.CURRENT_USER, AQTags.IS_USER_LOGGED_IN],
+      exact: false,
+    });
+
+    router.push("/sign-in");
+  };
+
   return (
-    <div className="px-4 py-3 flex gap-5 items-center justify-between bg-slate-800">
-      <div className="flex gap-3 items-center">
+    <div className="flex items-center justify-between gap-5 bg-slate-800 px-4 py-3">
+      <div className="flex items-center gap-3">
         <ASidebarDrawer />
         <Link href="/">
           <Image
@@ -16,6 +39,28 @@ const ATopbar = () => {
             height={35}
           />
         </Link>
+      </div>
+
+      <span className="hidden text-lg font-semibold uppercase text-slate-50 sm:block">
+        MG Admin Panel
+      </span>
+
+      <div className="flex items-center gap-3 text-sm">
+        <Link
+          href="/admin/users/profile"
+          className="flex items-center gap-1 text-slate-50"
+        >
+          <User className="size-5" />
+          <span>Profile</span>
+        </Link>
+
+        <div
+          className="flex cursor-pointer items-center gap-1 text-slate-50"
+          onClick={logout}
+        >
+          <LogOut className="size-5" />
+          <span>Logout</span>
+        </div>
       </div>
     </div>
   );
