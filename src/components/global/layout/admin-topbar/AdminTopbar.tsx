de75d6626b545b89ee7ff3dd61415c3d47d90ec4
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-11 12:12:42 +0600

    feat: enhance AdminTopbar with product management options and improved styling

--- src/components/global/layout/admin-topbar/AdminTopbar.tsx
+++ src/components/global/layout/admin-topbar/AdminTopbar.tsx
@@ -2,9 +2,16 @@
 
 import { USER_ROLE } from "@/constants";
 import { useIsUserLoggedInQuery } from "@/lib/queries/auth.query";
+import { LayoutDashboard, Plus, Settings, Trash2 } from "lucide-react";
 import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+import ProductDeleteDialog from "../../shared/ProductDeleteDialog";
+import ProductEditDropDown from "./ProductEditDropDown";
 
 const AdminTopbar = () => {
+  const pathName = usePathname();
+  const params = useParams();
+
   const { data: user, isLoading } = useIsUserLoggedInQuery();
 
   if (isLoading) return null;
@@ -18,10 +25,40 @@ const AdminTopbar = () => {
       <div className="flex">
         <Link
           href="/admin"
-          className="px-3 py-1 text-slate-50 hover:bg-slate-500"
+          className="flex items-center gap-1 px-3 py-1.5 text-slate-50 hover:bg-slate-600"
         >
-          Dashboard
+          <LayoutDashboard fill="#f8fafc" className="size-4" />
+          <span>Admin Dashboard</span>
         </Link>
+
+        <Link
+          href="/admin/settings/homepage"
+          className="flex items-center gap-1 px-3 py-1.5 text-slate-50 hover:bg-slate-600"
+        >
+          <Settings className="size-4" />
+          <span>Settings</span>
+        </Link>
+
+        {pathName.includes("/products/") && (
+          <>
+            <Link
+              href="/admin/products/add-product"
+              className="flex items-center gap-1 px-3 py-1.5 text-slate-50 hover:bg-slate-600"
+            >
+              <Plus className="size-4" />
+              <span>Add Product</span>
+            </Link>
+
+            <ProductEditDropDown />
+
+            <ProductDeleteDialog products={[`${params?._id}`]}>
+              <div className="flex cursor-pointer items-center gap-1 px-3 py-1.5 text-slate-50 hover:bg-slate-600">
+                <Trash2 className="size-4" />
+                <span>Delete</span>
+              </div>
+            </ProductDeleteDialog>
+          </>
+        )}
       </div>
     </div>
   );
