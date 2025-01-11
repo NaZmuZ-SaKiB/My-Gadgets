61776fb52edc5d8fae39bf5e545b25334178df4f
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-11 11:38:42 +0600

    feat: add floating cart button for easy access for user

--- src/components/global/layout/header/Cart.tsx
+++ src/components/global/layout/header/Cart.tsx
@@ -31,30 +31,46 @@ const Cart = () => {
   return (
     <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger className="cursor-pointer">
-        <div className="h-full relative">
+        <div className="relative h-full">
           <Image
             src={icons.cart}
             alt="cart"
-            className="size-6 mx-auto max-xl:relative max-xl:top-1"
+            className="mx-auto size-6 max-xl:relative max-xl:top-1"
           />
-          <span className="hidden xl:block text-sm">Cart</span>
+          <span className="hidden text-sm xl:block">Cart</span>
 
           {mounted && (
-            <span className="absolute size-[22px] text-[11px] text-white bg-slate-600 rounded-full grid place-items-center -top-2 -right-3">
+            <span className="absolute -right-3 -top-2 grid size-[22px] place-items-center rounded-full bg-slate-600 text-[11px] text-white">
+              {cart.length}
+            </span>
+          )}
+        </div>
+        <div className="fixed bottom-5 right-5 z-50 hidden size-14 flex-col items-center justify-center gap-1 rounded-md border border-slate-600 bg-slate-50 p-1 text-slate-50 xl:flex">
+          <Image
+            src={icons.cart}
+            alt="cart"
+            className="mx-auto size-5 max-xl:relative max-xl:top-1"
+          />
+          <span className="text-center text-xs font-semibold text-slate-700">
+            Cart
+          </span>
+
+          {mounted && (
+            <span className="absolute -right-2 -top-3 grid size-6 place-items-center rounded-full bg-slate-600 text-xs text-slate-50">
               {cart.length}
             </span>
           )}
         </div>
       </SheetTrigger>
-      <SheetContent className="px-3 py-4 z-[110] flex flex-col gap-4">
-        <SheetHeader className="flex flex-row justify-between items-start">
+      <SheetContent className="z-[110] flex flex-col gap-4 px-3 py-4">
+        <SheetHeader className="flex flex-row items-start justify-between">
           <SheetTitle className="text-2xl text-slate-700">Cart</SheetTitle>
           <SheetClose className="!mt-0">
-            <X className="size-6 p-0.5 text-slate-500 border rounded-full border-slate-500" />
+            <X className="size-6 rounded-full border border-slate-500 p-0.5 text-slate-500" />
           </SheetClose>
         </SheetHeader>
 
-        <div className="flex-1 flex flex-col gap-3 overflow-y-auto">
+        <div className="flex flex-1 flex-col gap-3 overflow-y-auto">
           {cart.map((item) => (
             <div key={`cart-${item._id}`} className="flex items-center gap-3">
               <Image src={item.image} alt={item.name} height={70} width={70} />
@@ -63,13 +79,13 @@ const Cart = () => {
                   {item.name}
                 </h3>
 
-                <div className="mt-2 flex justify-between items-center gap-3">
+                <div className="mt-2 flex items-center justify-between gap-3">
                   <p className="text-sm">
                     {item.quantity} x {formatCurrency(item.price)}
                   </p>
 
                   <Trash2
-                    className="size-5 text-red-500 cursor-pointer"
+                    className="size-5 cursor-pointer text-red-500"
                     onClick={() => removeFromCart(item._id)}
                   />
                 </div>
@@ -78,9 +94,9 @@ const Cart = () => {
           ))}
         </div>
 
-        <SheetFooter className="w-full block">
-          <div className="flex justify-between items-center">
-            <p className="text-lg font-semibold text-slate-700 gap-2 flex-wrap">
+        <SheetFooter className="block w-full">
+          <div className="flex items-center justify-between">
+            <p className="flex-wrap gap-2 text-lg font-semibold text-slate-700">
               Total
             </p>
             <p className="text-2xl font-semibold text-primary">
@@ -88,7 +104,7 @@ const Cart = () => {
             </p>
           </div>
 
-          <div className="!ml-0 flex gap-2 justify-between mt-3">
+          <div className="!ml-0 mt-3 flex justify-between gap-2">
             <Link
               href="/cart"
               className="flex-1"
