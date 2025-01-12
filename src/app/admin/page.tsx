899d7d2e4f1d2b7a0787545c6cd78d30a57dd781
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-12 22:00:18 +0600

    feat: update AdminDashboardPage to include Cards and TopSellingProducts components

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,17 @@
+import APageContainer from "@/components/admin/admin-ui/APageContainer";
+import APageHeading from "@/components/admin/admin-ui/APageHeading";
+import Cards from "./_components/Cards";
+import TopSellingProducts from "./_components/TopSellingProducts";
+
 const AdminDashboardPage = () => {
   return (
-    <div>
-      <h1>AdminDashboardPage</h1>
-    </div>
+    <APageContainer>
+      <APageHeading title="Dashboard" />
+
+      <Cards />
+
+      <TopSellingProducts />
+    </APageContainer>
   );
 };
 
