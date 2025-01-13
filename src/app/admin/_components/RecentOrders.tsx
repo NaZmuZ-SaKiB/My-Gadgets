e7749b69093c5324eb16d7ae332ee50162bba371
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-13 19:08:05 +0600

    feat: add RecentOrders component to display the latest orders in a floating box

---
+++ src/app/admin/_components/RecentOrders.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import AFloatingBox from "@/components/admin/admin-ui/AFloatingBox";
+import OrdersTable from "@/components/admin/shared/OrdersTable";
+import { useOrderGetAllQuery } from "@/lib/queries/order.query";
+import { TOrder } from "@/types/order.type";
+
+const RecentOrders = () => {
+  const { data: ordersData, isLoading } = useOrderGetAllQuery(`limit=5`);
+  const orders: TOrder[] = ordersData?.data;
+
+  return (
+    <div>
+      <AFloatingBox className="mb-4">
+        <h2 className="text-lg font-medium text-slate-700">Recent Orders</h2>
+      </AFloatingBox>
+      <OrdersTable orders={orders} isLoading={isLoading} />
+    </div>
+  );
+};
+
+export default RecentOrders;
