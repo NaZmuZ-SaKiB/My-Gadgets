26238751f93a7ff6a946f73a177b1dbedfbbc457
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-13 18:47:19 +0600

    feat: integrate SalesChart and OrdersPieChart into AdminDashboardPage

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,6 +2,10 @@ import APageContainer from "@/components/admin/admin-ui/APageContainer";
 import APageHeading from "@/components/admin/admin-ui/APageHeading";
 import Cards from "./_components/Cards";
 import TopSellingProducts from "./_components/TopSellingProducts";
+import AGrid from "@/components/admin/admin-ui/AGrid";
+import AFloatingBox from "@/components/admin/admin-ui/AFloatingBox";
+import SalesChart from "./_components/SalesChart";
+import OrdersPieChart from "./_components/OrdersPieChart";
 
 const AdminDashboardPage = () => {
   return (
@@ -11,6 +15,23 @@ const AdminDashboardPage = () => {
       <Cards />
 
       <TopSellingProducts />
+
+      <AGrid reverse>
+        <AFloatingBox>
+          <h2 className="mb-5 text-lg font-medium text-slate-700">
+            Revenue Report
+          </h2>
+
+          <SalesChart />
+        </AFloatingBox>
+        <AFloatingBox>
+          <h2 className="text-lg font-medium text-slate-700">
+            Orders Overview
+          </h2>
+
+          <OrdersPieChart />
+        </AFloatingBox>
+      </AGrid>
     </APageContainer>
   );
 };
