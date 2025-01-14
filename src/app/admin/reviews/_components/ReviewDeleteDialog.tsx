b8974c341b3470fe342d6a9df0e28fee1ed68773
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-14 12:03:18 +0600

    feat: add ReviewDeleteDialog component for confirming review deletions

---
+++ src/app/admin/reviews/_components/ReviewDeleteDialog.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { AQTags } from "@/constants";
+import { useReviewDeleteMutation } from "@/lib/queries/review.query";
+import { useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
+
+type TProps = {
+  id: string;
+  children?: React.ReactNode;
+};
+
+const ReviewDeleteDialog = ({ id, children }: TProps) => {
+  const [open, setOpen] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+
+  const queryClient = useQueryClient();
+
+  const { mutateAsync: deleteReview, isPending } = useReviewDeleteMutation();
+
+  const handleDelete = async () => {
+    try {
+      const result = await deleteReview(id);
+
+      if (result?.success) {
+        queryClient.invalidateQueries({
+          queryKey: [AQTags.REVIEW, AQTags.ALL],
+          exact: false,
+        });
+
+        toast.success(result?.message);
+      } else {
+        toast.error(result?.message || "A server error occurred.");
+      }
+    } catch (error: any) {
+      toast.error(error?.message || "A client error occurred.");
+    } finally {
+      setOpen(false);
+    }
+  };
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are you sure you want to delete this review?
+          </AlertDialogTitle>
+
+          <AlertDialogDescription>
+            You can not reverse this. Do you understand that?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <label className="flex items-center gap-2">
+          <Input
+            type="checkbox"
+            className="size-4"
+            onChange={(e) => setDisabled(!e.target.checked)}
+          />{" "}
+          <span className="text-slate-700">Yes, I understand.</span>
+        </label>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <Button
+            disabled={disabled || isPending}
+            variant="destructive"
+            onClick={handleDelete}
+          >
+            {isPending ? "Deleting..." : "Delete"}
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default ReviewDeleteDialog;
