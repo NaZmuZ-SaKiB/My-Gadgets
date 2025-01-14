540d44769c7b7122b2b0cff64b875ab093b2e36e
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-14 12:03:30 +0600

    feat: implement ReviewsData component with loading state and review card display

--- src/app/admin/reviews/_components/ReviewsData.tsx
+++ src/app/admin/reviews/_components/ReviewsData.tsx
@@ -2,24 +2,34 @@
 
 import AFloatingBox from "@/components/admin/admin-ui/AFloatingBox";
 import DataLimitSelect from "@/components/admin/shared/filters/DataLimitSelect";
-import DataSearchBox from "@/components/admin/shared/filters/DataSearchBox";
 import DataSortBySelect from "@/components/admin/shared/filters/DataSortBySelect";
 import DataSortOrderSelect from "@/components/admin/shared/filters/DataSortOrderSelect";
-import SelectedItemsCount from "@/components/admin/shared/SelectedItemsCount";
 import { reviewSortOptions } from "@/constants";
-import ReviewsTable from "./ReviewsTable";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { useReviewGetAllQuery } from "@/lib/queries/review.query";
+import { TReview } from "@/types/review.type";
+import ReviewCard from "./ReviewCard";
+import { Loader2 } from "lucide-react";
+import MGPagination from "@/components/global/shared/MGPagination";
 
 const ReviewsData = () => {
-  const [selectedReviews, setSelectedReviews] = useState<string[]>([]);
+  const searchParams = useSearchParams();
+  const { data, isLoading } = useReviewGetAllQuery(searchParams.toString());
+  const reviews: TReview[] = data?.data || [];
+
+  if (isLoading) {
+    return (
+      <div className="grid flex-1 place-items-center">
+        <Loader2 className="mx-auto size-[50px] animate-spin text-primary-hover" />
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col gap-3">
       <AFloatingBox className="flex flex-col gap-2">
-        <div className="flex gap-2 flex-wrap">
-          <SelectedItemsCount count={selectedReviews.length} />
-
-          {/* Review Delete Dialog  */}
-
+        <div className="flex flex-wrap gap-2">
           <div className="sm:hidden">
             <DataLimitSelect />
           </div>
@@ -28,14 +38,25 @@ const ReviewsData = () => {
 
           <DataSortOrderSelect />
         </div>
-
-        <DataSearchBox />
       </AFloatingBox>
 
-      <ReviewsTable
-        selectedReviews={selectedReviews}
-        setSelectedReviews={setSelectedReviews}
-      />
+      <div className="flex flex-wrap gap-3">
+        {reviews.map((review) => (
+          <ReviewCard key={`${review._id}`} review={review} />
+        ))}
+      </div>
+
+      <div className="mt-5 flex items-center justify-center gap-3 sm:justify-between">
+        <div className="max-sm:hidden">
+          <DataLimitSelect />
+        </div>
+        <MGPagination
+          admin
+          limit={data?.meta?.limit as number}
+          page={data?.meta?.page as number}
+          total={data?.meta?.total as number}
+        />
+      </div>
     </div>
   );
 };
