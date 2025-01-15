43cd2992ac6dafc66c097e8a7fb2dd2825835267
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-15 11:27:55 +0600

    feat: enhance MediaData component to display selected image with editing options

--- src/app/admin/media/_components/MediaData.tsx
+++ src/app/admin/media/_components/MediaData.tsx
@@ -5,12 +5,14 @@ import DataSearchBox from "@/components/admin/shared/filters/DataSearchBox";
 import ImageGallery from "./ImageGallery";
 import { useState } from "react";
 import { TMedia } from "@/types/media.type";
+import Image from "next/image";
+import ImageEditForm from "./ImageEditForm";
 
 const MediaData = () => {
   const [selectedImage, setSelectedImage] = useState<TMedia | null>(null);
 
   return (
-    <div className="flex gap-4">
+    <div className="flex items-start gap-4">
       <div>
         <AFloatingBox className="mb-5">
           <DataSearchBox />
@@ -21,7 +23,20 @@ const MediaData = () => {
         />
       </div>
 
-      {selectedImage && <AFloatingBox></AFloatingBox>}
+      {selectedImage && (
+        <AFloatingBox className="flex min-w-[300px] flex-col gap-3">
+          <div className="mx-auto">
+            <Image
+              src={selectedImage.secureUrl}
+              width={150}
+              height={150}
+              alt={selectedImage.name}
+              className="object-contain"
+            />
+          </div>
+          <ImageEditForm selectedImage={selectedImage} />
+        </AFloatingBox>
+      )}
     </div>
   );
 };
