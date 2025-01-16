34ef5958aa0a4b98fd1150b088e1ea63d62b9685
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-16 23:15:34 +0600

    feat: add Compare component to display comparison link and item count

---
+++ src/components/global/layout/header/Compare.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { icons } from "@/constants";
+import { useCompare } from "@/lib/providers/ContextProvider";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const Compare = () => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const { compare } = useCompare();
+  return (
+    <div className="relative">
+      <Link
+        href="/compare"
+        className="hidden cursor-pointer text-slate-600 hover:text-slate-800 xl:block"
+      >
+        <Image src={icons.compare} alt="compare" className="mx-auto size-6" />
+        <span className="text-sm">Compare</span>
+      </Link>
+
+      {mounted && (
+        <span className="absolute -top-2 right-0 grid size-[22px] place-items-center rounded-full bg-slate-600 text-[11px] text-white">
+          {compare.length}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default Compare;
