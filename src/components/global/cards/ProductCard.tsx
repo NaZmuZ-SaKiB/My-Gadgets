cd24cf506132568959f8d50abf489fa1692d4fd6
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-16 23:10:08 +0600

    feat: integrate CompareButton and Heart icon into ProductCard for enhanced product interaction

--- src/components/global/cards/ProductCard.tsx
+++ src/components/global/cards/ProductCard.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import AddToCartButton from "../shared/ProductCardAddToCart";
+import { Heart } from "lucide-react";
+import CompareButton from "../shared/CompareButton";
 
 type TProps = {
   product: TProduct;
@@ -23,34 +25,34 @@ const ProductCard = ({ product, showDescription = true }: TProps) => {
   const discount = Math.floor(
     ((Number(product.regularPrice) - Number(product.salePrice)) /
       Number(product.regularPrice)) *
-      100
+      100,
   );
 
   return (
-    <div className="flex flex-col border border-slate-200 rounded-xl overflow-hidden hover:border-primary-2 relative h-full">
-      <div className="relative w-full aspect-square">
+    <div className="hover:border-primary-2 group relative flex h-full flex-col overflow-hidden rounded-xl border border-slate-200">
+      <div className="relative aspect-square w-full">
         <Image
           src={product?.images?.[0].secureUrl}
           alt={product?.model}
           fill
-          className="p-2 sm:p-6 sm:pb-0 aspect-square object-contain"
+          className="aspect-square object-contain p-2 sm:p-6 sm:pb-0"
         />
       </div>
 
       <div
-        className={cn("p-2 sm:p-5 sm:pt-2 flex-1 flex flex-col", {
+        className={cn("flex flex-1 flex-col p-2 sm:p-5 sm:pt-2", {
           "justify-between gap-3": !showDescription,
         })}
       >
         <Link href={`/products/${product.slug}/${product._id}`}>
-          <h3 className="font-semibold text-slate-700 max-xs:text-sm hover:underline underline-offset-2 cursor-pointer">
+          <h3 className="cursor-pointer font-semibold text-slate-700 underline-offset-2 hover:underline max-xs:text-sm">
             {product?.name}
           </h3>
         </Link>
 
         <div
           className={cn({
-            "flex-1 flex flex-col justify-between": showDescription,
+            "flex flex-1 flex-col justify-between": showDescription,
           })}
         >
           {showDescription && (
@@ -58,16 +60,16 @@ const ProductCard = ({ product, showDescription = true }: TProps) => {
               dangerouslySetInnerHTML={{
                 __html: product.shortDescription,
               }}
-              className="mt-3 ml-3 mb-3 max-sm:px-3 text-xs text-gray-700 leading-5 [&>ul]:list-disc"
+              className="mb-3 ml-3 mt-3 text-xs leading-5 text-gray-700 max-sm:px-3 [&>ul]:list-disc"
             />
           )}
 
-          <div className="flex max-xs:flex-col max-xs:items-center justify-between gap-4 items-end">
-            <div className="flex items-center gap-x-3 flex-wrap">
-              <span className="font-bold text-primary text-xl">
+          <div className="flex items-end justify-between gap-4 max-xs:flex-col max-xs:items-center">
+            <div className="flex flex-wrap items-center gap-x-3">
+              <span className="text-xl font-bold text-primary">
                 {formatCurrency(product.salePrice)}
               </span>
-              <span className="text-slate-400 line-through decoration-2 font-semibold text-sm">
+              <span className="text-sm font-semibold text-slate-400 line-through decoration-2">
                 {formatCurrency(product.regularPrice)}
               </span>
             </div>
@@ -77,10 +79,17 @@ const ProductCard = ({ product, showDescription = true }: TProps) => {
       </div>
 
       <span
-        className={`absolute top-0 left-0 text-white text-sm font-semibold py-1.5 px-4 rounded-br-2xl ${color}`}
+        className={`absolute left-0 top-0 rounded-br-2xl px-4 py-1.5 text-sm font-semibold text-white ${color}`}
       >
         {product.badgeText || "- " + discount + "%"}
       </span>
+
+      <div className="absolute -right-full top-0 flex flex-col items-center bg-white text-gray-500 transition-all group-hover:right-0">
+        <span className="p-2">
+          <Heart />
+        </span>
+        <CompareButton product={product} />
+      </div>
     </div>
   );
 };
