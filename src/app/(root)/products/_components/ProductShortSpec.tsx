e7356e291e3a10862fa2d85029b7eb298034f5d9
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-19 00:12:11 +0600

    feat: integrate CompareButton into ProductShortSpec for enhanced product comparison functionality

--- src/app/(root)/products/_components/ProductShortSpec.tsx
+++ src/app/(root)/products/_components/ProductShortSpec.tsx
@@ -6,6 +6,7 @@ import { Heart, Shuffle } from "lucide-react";
 import QuantityInput from "./QuantityInput";
 import StarRatingComponent from "@/components/global/shared/StarRatingComponent";
 import { TReview } from "@/types/review.type";
+import CompareButton from "@/components/global/shared/CompareButton";
 
 type TProps = {
   product: TProduct;
@@ -16,7 +17,7 @@ const ProductShortSpec = ({ product, reviews }: TProps) => {
   const discount = Math.floor(
     ((Number(product.regularPrice) - Number(product.salePrice)) /
       Number(product.regularPrice)) *
-      100
+      100,
   );
 
   const avgRating =
@@ -24,7 +25,7 @@ const ProductShortSpec = ({ product, reviews }: TProps) => {
 
   return (
     <div>
-      <h1 className="text-3xl font-semibold text-slate-700 leading-10">
+      <h1 className="text-3xl font-semibold leading-10 text-slate-700">
         {product.name}
       </h1>
 
@@ -50,10 +51,10 @@ const ProductShortSpec = ({ product, reviews }: TProps) => {
       </div>
 
       {/* Stock Status  */}
-      <div className="mt-8 px-4 py-2 rounded-lg bg-slate-100 text-slate-500 font-semibold inline-flex gap-2">
+      <div className="mt-8 inline-flex gap-2 rounded-lg bg-slate-100 px-4 py-2 font-semibold text-slate-500">
         <span>Availability:</span>
         <span
-          className={cn("text-slate-700 font-bold", {
+          className={cn("font-bold text-slate-700", {
             "text-primary": product.quantity > 0,
           })}
         >
@@ -68,31 +69,33 @@ const ProductShortSpec = ({ product, reviews }: TProps) => {
           dangerouslySetInnerHTML={{
             __html: product.shortDescription,
           }}
-          className="text-sm ml-5 mt-2 leading-7 [&>ul]:list-disc"
+          className="ml-5 mt-2 text-sm leading-7 [&>ul]:list-disc"
         />
       </div>
 
       {/* Add to Cart Button  */}
-      <div className="mt-5 flex gap-2 flex-wrap">
+      <div className="mt-5 flex flex-wrap gap-2">
         <QuantityInput max={product.quantity} />
 
         <div className="flex gap-2">
           <div>
             <Button
               variant="outline"
-              className="focus-visible:ring-primary-1 h-full border-slate-300 text-slate-500 hover:bg-primary-1 hover:text-primary-3 px-3"
+              className="focus-visible:ring-primary-1 hover:bg-primary-1 hover:text-primary-3 h-full border-slate-300 px-3 text-slate-500"
             >
               <Heart />
             </Button>
           </div>
 
           <div>
-            <Button
-              variant="outline"
-              className="focus-visible:ring-primary-1 h-full border-slate-300 text-slate-500 hover:bg-primary-1 hover:text-primary-3 px-3"
-            >
-              <Shuffle />
-            </Button>
+            <CompareButton product={product} className="h-full text-slate-500">
+              <Button
+                variant="outline"
+                className="focus-visible:ring-primary-1 hover:bg-primary-1 hover:text-primary-3 h-full border-slate-300 px-3"
+              >
+                <Shuffle />
+              </Button>
+            </CompareButton>
           </div>
         </div>
       </div>
