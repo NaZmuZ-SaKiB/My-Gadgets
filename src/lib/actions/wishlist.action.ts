1d5276f9302ab8b2971bff672aefbcf29c571551
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-18 23:54:43 +0600

    feat: implement wishlist actions for fetching, adding, removing, and clearing items

---
+++ src/lib/actions/wishlist.action.ts
@@ -0,0 +1,62 @@
+import { authKey, backendUrl } from "@/constants";
+import { cookies } from "next/headers";
+
+export const wishlistGetAction = async () => {
+  const response = await fetch(`${backendUrl}/api/wishlist`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: (await cookies()).get(authKey)?.value || "",
+    },
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  return result;
+};
+
+export const wishlistAddAction = async (productId: string) => {
+  const response = await fetch(`${backendUrl}/api/wishlist/${productId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: (await cookies()).get(authKey)?.value || "",
+    },
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  return result;
+};
+
+export const wishlistRemoveAction = async (productId: string) => {
+  const response = await fetch(`${backendUrl}/api/wishlist/${productId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: (await cookies()).get(authKey)?.value || "",
+    },
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  return result;
+};
+
+export const wishlistClearAction = async () => {
+  const response = await fetch(`${backendUrl}/api/wishlist`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: (await cookies()).get(authKey)?.value || "",
+    },
+    cache: "no-store",
+  });
+
+  const result = await response.json();
+
+  return result;
+};
