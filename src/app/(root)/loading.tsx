36562718f3cc45d88cdfb34cda05d2f880d4ba60
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-19 19:16:42 +0600

    refactor: enhance loading component with improved styling and layout

--- src/app/(root)/loading.tsx
+++ src/app/(root)/loading.tsx
@@ -1,7 +1,14 @@
 const MainLoading = () => {
   return (
-    <div>
-      <h1>Loading...</h1>
+    <div className="mg-container">
+      <div className="pb-2 pt-4 sm:pb-4 md:pt-8">
+        <div className="aspect-[16/5] w-full animate-pulse overflow-hidden rounded-lg bg-slate-200 sm:rounded-2xl"></div>
+      </div>
+      <div className="flex gap-3">
+        <div className="aspect-[16/8] w-full animate-pulse overflow-hidden rounded-lg bg-slate-200 sm:rounded-2xl"></div>
+        <div className="aspect-[16/8] w-full animate-pulse overflow-hidden rounded-lg bg-slate-200 sm:rounded-2xl"></div>
+        <div className="aspect-[16/8] w-full animate-pulse overflow-hidden rounded-lg bg-slate-200 max-md:hidden sm:rounded-2xl"></div>
+      </div>
     </div>
   );
 };
