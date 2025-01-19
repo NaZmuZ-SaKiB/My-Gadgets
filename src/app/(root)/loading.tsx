56175f652e154e83d137ef90c7b463e103901561
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-19 19:42:38 +0600

    refactor: enhance loading component with featured categories section

--- src/app/(root)/loading.tsx
+++ src/app/(root)/loading.tsx
@@ -9,6 +9,23 @@ const MainLoading = () => {
         <div className="aspect-[16/8] w-full animate-pulse overflow-hidden rounded-lg bg-slate-200 sm:rounded-2xl"></div>
         <div className="aspect-[16/8] w-full animate-pulse overflow-hidden rounded-lg bg-slate-200 max-md:hidden sm:rounded-2xl"></div>
       </div>
+
+      <div className="pb-4 pt-4 md:pt-6">
+        <h2 className="text-2xl font-bold text-slate-700 md:text-3xl">
+          Featured Categories
+        </h2>
+
+        <div className="mt-5 flex flex-wrap gap-3 md:mt-8 md:gap-4">
+          {Array(12)
+            .fill(0)
+            .map((_, i) => (
+              <div
+                key={`featured-cats-loading-${i}`}
+                className="aspect-square max-h-[130px] flex-1 basis-32 animate-pulse rounded-xl bg-slate-200 sm:basis-48"
+              ></div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
