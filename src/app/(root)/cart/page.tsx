75afd9520e5cd151f90528016516f9ef2142b7f9
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-19 19:04:27 +0600

    refactor: improve layout and styling of CartPage component

--- src/app/(root)/cart/page.tsx
+++ src/app/(root)/cart/page.tsx
@@ -28,12 +28,12 @@ const CartPage = () => {
 
       <div className="mt-5 sm:mt-3 sm:rounded-xl sm:border sm:border-slate-200 sm:p-4">
         {cart.length > 0 && (
-          <div className="flex items-end gap-3 justify-between">
+          <div className="flex items-end justify-between gap-3">
             <div>
               <h1 className="text-3xl font-semibold text-slate-700">
                 Your Cart
               </h1>
-              <p className="text-slate-500 font-semibold mt-1 text-sm sm:text-base">
+              <p className="mt-1 text-sm font-semibold text-slate-500 sm:text-base">
                 You have <span className="text-primary">{cart.length}</span>{" "}
                 product
                 {cart.length > 1 ? "s" : ""} in the cart
@@ -41,7 +41,7 @@ const CartPage = () => {
             </div>
 
             <div
-              className="shrink-0 flex items-center gap-1 text-sm text-slate-500 border border-slate-500 rounded-lg px-2 py-1 cursor-pointer"
+              className="flex shrink-0 cursor-pointer items-center gap-1 rounded-lg border border-slate-500 px-2 py-1 text-sm text-slate-500"
               onClick={clearCart}
             >
               <Trash2 className="size-4" />
@@ -52,7 +52,7 @@ const CartPage = () => {
 
         <div className="mt-6">
           {cart.length > 0 ? (
-            <table className="table-auto admin-table rounded-md overflow-hidden">
+            <table className="primary-table table table-auto">
               <thead className="text-left">
                 <tr>
                   <th className="max-md:hidden">Image</th>
@@ -93,7 +93,7 @@ const CartPage = () => {
             </table>
           ) : (
             <div className="rounded-xl border border-slate-200 px-5 py-10">
-              <p className="text-3xl text-slate-400 font-bold text-center">
+              <p className="text-center text-3xl font-bold text-slate-400">
                 Cart is Empty
               </p>
             </div>
@@ -102,16 +102,16 @@ const CartPage = () => {
           {cart.length > 0 && (
             <div className="mt-5 flex justify-end">
               <div className="flex flex-col gap-2">
-                <p className="flex gap-5 items-end">
-                  <span className="md:text-lg font-semibold text-slate-700">
+                <p className="flex items-end gap-5">
+                  <span className="font-semibold text-slate-700 md:text-lg">
                     Total:
                   </span>
-                  <span className="col-span-2 text-xl md:text-2xl font-semibold text-primary">
+                  <span className="col-span-2 text-xl font-semibold text-primary md:text-2xl">
                     {formatCurrency(total)}
                   </span>
                 </p>
 
-                <Link href="/checkout" className="mt-3 self-end w-full">
+                <Link href="/checkout" className="mt-3 w-full self-end">
                   <MGButton className="w-full rounded-md">Checkout</MGButton>
                 </Link>
               </div>
