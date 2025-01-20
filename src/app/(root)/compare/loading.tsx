2630702c48e36f5e6ed9292d62459f49461fbc79
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-20 23:34:44 +0600

    refactor: add ComparePageLoading component for loading state in ComparePage

---
+++ src/app/(root)/compare/loading.tsx
@@ -0,0 +1,22 @@
+import BreadcrumbBar from "@/components/global/shared/Breadcrumb";
+
+const ComparePageLoading = () => {
+  return (
+    <div className="mg-container pt-4">
+      <BreadcrumbBar items={[{ label: "Compare" }]} />
+
+      <div className="mb-1 mt-4 h-40 w-full animate-pulse bg-slate-200"></div>
+
+      {Array(10)
+        .fill(0)
+        .map((_, i) => (
+          <div
+            key={`compare-table-loading-${i}`}
+            className="mb-1 h-10 w-full animate-pulse bg-slate-200"
+          ></div>
+        ))}
+    </div>
+  );
+};
+
+export default ComparePageLoading;
