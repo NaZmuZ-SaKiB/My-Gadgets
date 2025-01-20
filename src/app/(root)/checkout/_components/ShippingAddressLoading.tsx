4285088cc26575645f8a6393393d0a8163a94146
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-20 23:02:31 +0600

    refactor: add loading component for ShippingAddress

---
+++ src/app/(root)/checkout/_components/ShippingAddressLoading.tsx
@@ -0,0 +1,7 @@
+const ShippingAddressLoading = () => {
+  return (
+    <div className="animate-pulse rounded-xl bg-slate-200 max-lg:col-span-2 max-lg:h-[450px] max-lg:border-b max-lg:pb-4 sm:border sm:border-slate-200 sm:p-4 lg:row-span-2"></div>
+  );
+};
+
+export default ShippingAddressLoading;
