bbf1acb2371787c238fd5b00bd18920d1ed70ef2
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-24 16:37:19 +0600

    refactor: add EditProductPageLoading component for loading state in edit product page

---
+++ src/app/admin/products/[id]/loading.tsx
@@ -0,0 +1,12 @@
+import APageContainer from "@/components/admin/admin-ui/APageContainer";
+import APageHeading from "@/components/admin/admin-ui/APageHeading";
+
+const EditProductPageLoading = () => {
+  return (
+    <APageContainer>
+      <APageHeading title="Product" />
+    </APageContainer>
+  );
+};
+
+export default EditProductPageLoading;
