d2b52a7c18bb91539145e378550aa2a57320eacd
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-24 22:46:01 +0600

    refactor: improve layout and styling in SingleBrandPage component

--- src/app/admin/brands/[id]/page.tsx
+++ src/app/admin/brands/[id]/page.tsx
@@ -5,12 +5,10 @@ import AGrid from "@/components/admin/admin-ui/AGrid";
 import APageContainer from "@/components/admin/admin-ui/APageContainer";
 import APageHeading from "@/components/admin/admin-ui/APageHeading";
 import MGAInput from "@/components/admin/forms/MGAInput";
-import MGASelect, { TSelectOption } from "@/components/admin/forms/MGASelect";
 import MGForm from "@/components/global/forms/MGForm";
 import MGButton from "@/components/global/shared/MGButton";
 import { AQTags } from "@/constants";
 import {
-  useBrandGetAllQuery,
   useBrandGetByIdQuery,
   useBrandUpdateMutation,
 } from "@/lib/queries/brand.query";
@@ -32,7 +30,7 @@ const SingleBrandPage = () => {
   const queryClient = useQueryClient();
 
   const { data: brandData, isLoading: brandLoading } = useBrandGetByIdQuery(
-    id as string
+    id as string,
   );
   const brand: TBrand = brandData?.data;
 
@@ -66,8 +64,8 @@ const SingleBrandPage = () => {
 
   if (brandLoading) {
     return (
-      <div className="h-full grid place-items-center">
-        <Loader2 className="animate-spin mx-auto size-[100px] text-primary" />
+      <div className="grid h-full place-items-center">
+        <Loader2 className="mx-auto size-[100px] animate-spin text-primary" />
       </div>
     );
   }
@@ -89,7 +87,7 @@ const SingleBrandPage = () => {
             defaultValues={defaultValues}
             reset={false}
           >
-            <h2 className="font-medium text-slate-700 text-lg mb-1">
+            <h2 className="mb-1 text-lg font-medium text-slate-700">
               Update Brand
             </h2>
 
@@ -102,7 +100,7 @@ const SingleBrandPage = () => {
             />
 
             <MGButton
-              className="rounded-none self-start px-5 py-2 h-auto"
+              className="h-auto self-start rounded-none px-5 py-2"
               disabled={isPending}
             >
               {isPending ? "Updating..." : "Update Brand"}
@@ -112,7 +110,7 @@ const SingleBrandPage = () => {
               id={id as string}
               label="Featured"
               defaultValue={brand?.featured}
-              className="justify-between max-w-52"
+              className="max-w-52 justify-between"
             />
           </MGForm>
         </AFloatingBox>
