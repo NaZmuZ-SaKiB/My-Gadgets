b7d038acc69936b1054094d66ab9d3b416bc972f
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-24 22:46:07 +0600

    refactor: add BrandsPageLoading component for loading state in brands page

---
+++ src/app/admin/brands/loading.tsx
@@ -0,0 +1,12 @@
+import APageContainer from "@/components/admin/admin-ui/APageContainer";
+import APageHeading from "@/components/admin/admin-ui/APageHeading";
+
+const BrandsPageLoading = () => {
+  return (
+    <APageContainer>
+      <APageHeading title="Product Brands" />
+    </APageContainer>
+  );
+};
+
+export default BrandsPageLoading;
