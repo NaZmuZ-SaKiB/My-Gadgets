ec5a00bde6c048c658d1284e06eb2c51e45dedca
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-24 23:19:36 +0600

    refactor: move account folder to (account) folder

---
+++ src/app/(account)/account/layout.tsx
@@ -0,0 +1,30 @@
+import BreadcrumbBar from "@/components/global/shared/Breadcrumb";
+import AccountSidebar from "./_components/AccountSidebar";
+import AdminTopbar from "@/components/global/layout/admin-topbar/AdminTopbar";
+import FloatingCompareButton from "@/components/global/shared/FloatingCompareButton";
+import MobileBottombar from "@/components/global/layout/MobileBottombar";
+import Footer from "@/components/global/layout/footer/Footer";
+import Header from "@/components/global/layout/header/Header";
+
+const AccountLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <AdminTopbar />
+      <Header />
+      <div className="mg-container p-4">
+        <BreadcrumbBar items={[{ label: "My Account" }]} />
+
+        <div className="mt-5 grid gap-4 lg:grid-cols-[220px_1fr]">
+          <AccountSidebar />
+
+          <div className="overflow-x-auto">{children}</div>
+        </div>
+      </div>
+      <FloatingCompareButton />
+      <Footer />
+      <MobileBottombar />
+    </>
+  );
+};
+
+export default AccountLayout;
