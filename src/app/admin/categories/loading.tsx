f94623e347dcbc985dc3ed22c793620e26b7ac58
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-24 16:37:24 +0600

    refactor: add CategoriesPageLoading component for loading state in categories page

---
+++ src/app/admin/categories/loading.tsx
@@ -0,0 +1,12 @@
+import APageContainer from "@/components/admin/admin-ui/APageContainer";
+import APageHeading from "@/components/admin/admin-ui/APageHeading";
+
+const CategoriesPageLoading = () => {
+  return (
+    <APageContainer>
+      <APageHeading title="Product Categories" />
+    </APageContainer>
+  );
+};
+
+export default CategoriesPageLoading;
