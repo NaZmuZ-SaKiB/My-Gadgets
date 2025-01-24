c7513c52e1f6565d14db6138a8a0d8134dbe3799
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-24 22:46:13 +0600

    refactor: add BranchesPage component for branches management

---
+++ src/app/admin/branches/loading.tsx
@@ -0,0 +1,12 @@
+import APageContainer from "@/components/admin/admin-ui/APageContainer";
+import APageHeading from "@/components/admin/admin-ui/APageHeading";
+
+const BranchesPage = () => {
+  return (
+    <APageContainer>
+      <APageHeading title="Branches" />
+    </APageContainer>
+  );
+};
+
+export default BranchesPage;
