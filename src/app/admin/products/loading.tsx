668f376d70d13696634ce664e6cda82bde272217
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-24 16:15:36 +0600

    refactor: add AllProductsPageLoading component for loading state in products page

---
+++ src/app/admin/products/loading.tsx
@@ -0,0 +1,12 @@
+import APageContainer from "@/components/admin/admin-ui/APageContainer";
+import APageHeading from "@/components/admin/admin-ui/APageHeading";
+
+const AllProductsPageLoading = () => {
+  return (
+    <APageContainer>
+      <APageHeading title="Products" />
+    </APageContainer>
+  );
+};
+
+export default AllProductsPageLoading;
