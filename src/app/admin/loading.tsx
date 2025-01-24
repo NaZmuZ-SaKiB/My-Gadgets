8bc6859e61e98695e0f0fd1ed5207407be37b347
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-24 13:00:57 +0600

    refactor: add AdminDashboardLoading component for loading state in admin dashboard

---
+++ src/app/admin/loading.tsx
@@ -0,0 +1,23 @@
+import AFloatingBox from "@/components/admin/admin-ui/AFloatingBox";
+import APageContainer from "@/components/admin/admin-ui/APageContainer";
+import { cn } from "@/lib/utils";
+import CardsLoader from "./_components/CardsLoader";
+import TopSellingLoading from "./_components/TopSellingLoading";
+
+const AdminDashboardLoading = () => {
+  return (
+    <APageContainer>
+      <AFloatingBox className="flex justify-between">
+        <div className="flex items-center">
+          <h1 className={cn("text-xl font-bold text-slate-700")}>Dashboard</h1>
+        </div>
+      </AFloatingBox>
+
+      <CardsLoader />
+
+      <TopSellingLoading />
+    </APageContainer>
+  );
+};
+
+export default AdminDashboardLoading;
