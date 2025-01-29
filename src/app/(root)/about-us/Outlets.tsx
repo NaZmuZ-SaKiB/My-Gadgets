bb7ce601e77b25a78fbfa5a5bb0be8065125189f
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-29 19:01:47 +0600

    feat: improve responsive design of Outlets component with adjusted text sizes

--- src/app/(root)/about-us/Outlets.tsx
+++ src/app/(root)/about-us/Outlets.tsx
@@ -14,18 +14,18 @@ const Outlets = async () => {
           key={`${branch._id}`}
           className="rounded-xl bg-slate-100 px-6 py-5"
         >
-          <h3 className="mb-2 text-xl font-semibold text-slate-700">
+          <h3 className="mb-2 text-lg font-semibold text-slate-700 md:text-xl">
             {branch.name}
           </h3>
-          <p className="flex items-center gap-1 text-slate-500">
+          <p className="flex items-center gap-1 text-slate-500 max-md:text-sm">
             <MapPin className="size-4" />
             <span>{branch.address}</span>
           </p>
-          <p className="mt-2 flex items-center gap-1 text-slate-500">
+          <p className="mt-2 flex items-center gap-1 text-slate-500 max-md:text-sm">
             <Phone className="size-4" />
             <span>{branch.phone}</span>
           </p>
-          <p className="mt-2 flex items-center gap-1 text-slate-500">
+          <p className="mt-2 flex items-center gap-1 text-slate-500 max-md:text-sm">
             <MapPinned className="size-4" />
             <Link
               href={branch?.mapLink ?? "#"}
