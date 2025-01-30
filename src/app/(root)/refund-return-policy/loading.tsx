6dc77255d180a71e7ee8fc7d05d446bce694c48d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-30 15:09:45 +0600

    feat: add loading component for Refund & Return Policy page

---
+++ src/app/(root)/refund-return-policy/loading.tsx
@@ -0,0 +1,11 @@
+import { Loader2 } from "lucide-react";
+
+const RefundReturnPolicyLoading = () => {
+  return (
+    <div className="grid min-h-[90svh] place-items-center">
+      <Loader2 className="size-14 animate-spin text-primary" />
+    </div>
+  );
+};
+
+export default RefundReturnPolicyLoading;
