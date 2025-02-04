8ac23052977744ccc70a9473b352935575fa456a
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-04 13:10:25 +0600

    feat: add SearchResultPage component for displaying product search results

---
+++ src/app/(root)/shop/search/page.tsx
@@ -0,0 +1,82 @@
+import { Metadata } from "next";
+
+import ProductCard from "@/components/global/cards/ProductCard";
+import BreadcrumbBar from "@/components/global/shared/Breadcrumb";
+import MGPagination from "@/components/global/shared/MGPagination";
+
+import { productGetAllAction } from "@/lib/actions/product.action";
+import { brandGetAllAction } from "@/lib/actions/brand.action";
+import { TProduct } from "@/types/product.type";
+import { TBrand } from "@/types/brand.type";
+import BrandsFilter from "../_components/BrandsFilter";
+import Filters from "../_components/Filters";
+import FilterTopBar from "../_components/FilterTopBar";
+
+type TProps = {
+  searchParams: any;
+};
+
+export const metadata: Metadata = {
+  title: "Search Results",
+};
+
+const SearchResultPage = async (props: TProps) => {
+  const searchParams = await props.searchParams;
+  const limit = searchParams?.limit ?? 35;
+  if (searchParams?.limit) delete searchParams.limit;
+
+  const queries = new URLSearchParams(searchParams);
+
+  const productsData = await productGetAllAction(
+    `limit=${limit}&${queries.toString()}`,
+  );
+  const products: TProduct[] = productsData.data || [];
+
+  const brandsData = await brandGetAllAction(
+    "limit=100&sortBy=name&sortOrder=asc",
+  );
+  const brands: TBrand[] = brandsData.data || [];
+
+  const breadcrumbItems = [
+    {
+      label: "Shop - All Products",
+      link: null,
+    },
+  ];
+  return (
+    <div className="mg-container py-4">
+      <BreadcrumbBar items={breadcrumbItems} />
+
+      <BrandsFilter brands={brands} />
+
+      <div className="mt-3 grid-cols-[250px_1fr] gap-3 lg:grid">
+        <div className="bg-white max-lg:hidden">
+          <Filters />
+        </div>
+        <div>
+          <FilterTopBar />
+
+          <div className="mg-shop-grid mt-3">
+            {products.map((product) => (
+              <div key={`shop-product-${product._id}`} className="">
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {productsData?.meta?.limit <= productsData?.meta?.total && (
+        <div className="mt-5">
+          <MGPagination
+            limit={productsData?.meta?.limit as number}
+            page={productsData?.meta?.page as number}
+            total={productsData?.meta?.total as number}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SearchResultPage;
