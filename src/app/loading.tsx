57efbb33a4d66a69b8dee116e077ffd3947576d8
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-04 22:14:27 +0600

    feat: enhance loading component with logo and custom font

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,28 @@
-import { Loader2 } from "lucide-react";
+import { images } from "@/constants";
+import { cn } from "@/lib/utils";
+import { Permanent_Marker } from "next/font/google";
+import Image from "next/image";
+
+const font = Permanent_Marker({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 const MainLoading = () => {
   return (
-    <div className="grid min-h-[90svh] place-items-center">
-      <Loader2 className="size-14 animate-spin text-primary" />
+    <div className="grid min-h-[95svh] place-items-center">
+      <div className="flex items-center gap-2">
+        <Image
+          src={images.logo}
+          className="max-w-10 max-xl:h-10 max-sm:size-8"
+          alt="logo"
+        />
+        <span
+          className={cn(font.className, "text-xl font-semibold sm:text-2xl")}
+        >
+          My Gadgets
+        </span>
+      </div>
     </div>
   );
 };
