efb8daf2a7af3da5cf867b2c3771196c58a46e63
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-07 22:51:17 +0600

    feat: add ProductsGrid component for displaying product listings with pagination

---
+++ src/app/(root)/shop/_components/ProductsGrid.tsx
@@ -0,0 +1,43 @@
+import ProductCard from "@/components/global/cards/ProductCard";
+import MGPagination from "@/components/global/shared/MGPagination";
+
+import { productGetAllAction } from "@/lib/actions/product.action";
+import { TProduct } from "@/types/product.type";
+
+type TProps = {
+  searchParams: URLSearchParams;
+};
+
+const ProductsGrid = async (props: TProps) => {
+  const searchParams = props.searchParams;
+  const limit = searchParams.get("limit") ?? 35;
+
+  const queries = new URLSearchParams(searchParams);
+
+  const productsData = await productGetAllAction(
+    `limit=${limit}&${queries.toString()}`,
+  );
+  const products: TProduct[] = productsData.data || [];
+  return (
+    <>
+      <div className="mg-shop-grid mt-3">
+        {products.map((product) => (
+          <div key={`shop-product-${product._id}`} className="">
+            <ProductCard product={product} />
+          </div>
+        ))}
+      </div>
+      {productsData?.meta?.limit <= productsData?.meta?.total && (
+        <div className="mt-5">
+          <MGPagination
+            limit={productsData?.meta?.limit as number}
+            page={productsData?.meta?.page as number}
+            total={productsData?.meta?.total as number}
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ProductsGrid;
