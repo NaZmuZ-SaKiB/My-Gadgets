65f3699c413487d211a31ba8cc2bd78fc3af5a72
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-08 13:04:49 +0600

    feat: refactor BrandsFilter component to use brandGetAllAction and delegate rendering to BrandsFilterItems

--- src/app/(root)/shop/_components/BrandsFilter.tsx
+++ src/app/(root)/shop/_components/BrandsFilter.tsx
@@ -1,84 +1,14 @@
-"use client";
-
-import Image from "next/image";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-
-import { cn } from "@/lib/utils";
+import { brandGetAllAction } from "@/lib/actions/brand.action";
 import { TBrand } from "@/types/brand.type";
-import { useBrandGetAllQuery } from "@/lib/queries/brand.query";
+import BrandsFilterItems from "./BrandsFilterItems";
 
-const BrandsFilter = () => {
-  const pathName = usePathname();
-  const searchParams = useSearchParams();
-
-  const { data: brandsData, isLoading } = useBrandGetAllQuery(
+const BrandsFilter = async () => {
+  const brandsData = await brandGetAllAction(
     "limit=100&sortBy=name&sortOrder=asc",
   );
   const brands: TBrand[] = brandsData?.data || [];
 
-  const router = useRouter();
-
-  const handleChange = (value: string) => {
-    const params = new URLSearchParams(searchParams);
-    params.delete("page");
-    params.delete("brand");
-
-    params.append("page", "1");
-    if (value !== "all") {
-      params.append("brand", value);
-    }
-
-    router.replace(`${pathName}?${params}`);
-  };
-
-  if (isLoading) {
-    return (
-      <div className="mt-3 h-14 animate-pulse rounded-xl bg-slate-100"></div>
-    );
-  }
-
-  return (
-    <div className="no-scrollbar mt-3 flex justify-start gap-2 overflow-y-auto">
-      <div
-        className={cn(
-          "grid shrink-0 cursor-pointer place-items-center rounded-md border px-4 py-1.5",
-          {
-            "border-primary": !searchParams.get("brand"),
-          },
-        )}
-        onClick={() => handleChange("all")}
-      >
-        <span className="text-xs font-semibold text-slate-700">All Brands</span>
-      </div>
-
-      {brands.map((brand) => (
-        <div
-          key={`${brand._id}`}
-          className={cn(
-            "grid shrink-0 cursor-pointer place-items-center rounded-md border px-4 py-1.5",
-            {
-              "border-primary": searchParams.get("brand") === brand.name,
-            },
-          )}
-          onClick={() => handleChange(brand.name)}
-        >
-          {brand.image ? (
-            <Image
-              src={brand.image?.secureUrl}
-              alt={brand.name}
-              width={40}
-              height={40}
-              className="object-contain"
-            />
-          ) : (
-            <span className="text-xs font-semibold text-slate-700">
-              {brand.name}
-            </span>
-          )}
-        </div>
-      ))}
-    </div>
-  );
+  return <BrandsFilterItems brands={brands} />;
 };
 
 export default BrandsFilter;
