78fa5825f7e7629b01213f8b4af34c7f7c87776b
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-08 12:48:45 +0600

    feat: refactor ShopPage to utilize ProductsGrid with improved loading state and remove unused product fetching logic

--- src/app/(root)/shop/[category]/page.tsx
+++ src/app/(root)/shop/[category]/page.tsx
@@ -1,16 +1,12 @@
+import { Suspense } from "react";
 import { Metadata, ResolvingMetadata } from "next";
 
 import Filters from "../_components/Filters";
-import { TProduct } from "@/types/product.type";
 import BrandsFilter from "../_components/BrandsFilter";
 import FilterTopBar from "../_components/FilterTopBar";
-import ProductCard from "@/components/global/cards/ProductCard";
+import ProductsGrid from "../_components/ProductsGrid";
+import ProductsLoading from "../_components/ProductsLoading";
 import BreadcrumbBar from "@/components/global/shared/Breadcrumb";
-import MGPagination from "@/components/global/shared/MGPagination";
-
-import { TBrand } from "@/types/brand.type";
-import { brandGetAllAction } from "@/lib/actions/brand.action";
-import { productGetAllAction } from "@/lib/actions/product.action";
 
 type TProps = {
   params: Promise<{
@@ -37,25 +33,13 @@ export async function generateMetadata(
 
 const ShopPage = async (props: TProps) => {
   const searchParams = await props.searchParams;
-  const params = await props.params;
-  const limit = searchParams?.limit ?? 35;
-  if (searchParams?.limit) delete searchParams.limit;
+  const params = new URLSearchParams(searchParams);
 
-  const queries = new URLSearchParams(searchParams);
-
-  const productsData = await productGetAllAction(
-    `limit=${limit}&category=${params.category}&${queries.toString()}`,
-  );
-  const products: TProduct[] = productsData.data || [];
-
-  const brandsData = await brandGetAllAction(
-    "limit=100&sortBy=name&sortOrder=asc",
-  );
-  const brands: TBrand[] = brandsData.data || [];
+  const category = (await props.params).category;
 
   const breadcrumbItems = [
     {
-      label: params.category as string,
+      label: category as string,
       link: null,
     },
   ];
@@ -64,7 +48,7 @@ const ShopPage = async (props: TProps) => {
     <div className="mg-container py-4">
       <BreadcrumbBar items={breadcrumbItems} />
 
-      <BrandsFilter brands={brands} />
+      <BrandsFilter />
 
       <div className="mt-3 grid-cols-[250px_1fr] gap-3 lg:grid">
         <div className="bg-white max-lg:hidden">
@@ -73,25 +57,14 @@ const ShopPage = async (props: TProps) => {
         <div>
           <FilterTopBar />
 
-          <div className="mg-shop-grid mt-3">
-            {products.map((product) => (
-              <div key={`shop-product-${product._id}`} className="">
-                <ProductCard product={product} />
-              </div>
-            ))}
-          </div>
+          <Suspense
+            key={`${params.toString()}-${Math.random()}`}
+            fallback={<ProductsLoading />}
+          >
+            <ProductsGrid searchParams={searchParams} category={category} />
+          </Suspense>
         </div>
       </div>
-
-      {productsData?.meta?.limit <= productsData?.meta?.total && (
-        <div className="mt-5">
-          <MGPagination
-            limit={productsData?.meta?.limit as number}
-            page={productsData?.meta?.page as number}
-            total={productsData?.meta?.total as number}
-          />
-        </div>
-      )}
     </div>
   );
 };
