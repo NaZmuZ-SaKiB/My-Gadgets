e7e3d03cce391670c8ac861060384c167732b699
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-09 10:49:01 +0600

    feat: add NoProducts component to display message when no products are found

---
+++ src/app/(root)/shop/_components/NoProducts.tsx
@@ -0,0 +1,9 @@
+const NoProducts = () => {
+  return (
+    <div className="grid h-[150px] place-items-center rounded-xl border p-4">
+      <p className="text-xl font-semibold text-slate-500">No Products Found</p>
+    </div>
+  );
+};
+
+export default NoProducts;
